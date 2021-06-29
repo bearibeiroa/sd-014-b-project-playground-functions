20a27aa4bdada3f89202f060569620e30a8e7039
Mairton Damasceno <mairtondamasceno@gmail.com>
2021-06-29 17:42:55 -0300

    Atividades 03, 04, 05 e 06.

--- src/challenges.js
+++ src/challenges.js
@@ -23,19 +23,44 @@ function splitSentence(frase) {
 console.log(splitSentence("go ty be"))
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+//Isso quer dizer que, caso o parâmetro passado para concatName seja a Array ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'], a função deverá retornar Paolillo, Lucas.
+
+function concatName(frase) {
+  // result = frase[frase.length - 1] + frase[0];
+  // return result
+  result = frase[frase.length - 1]
+  result += ", "
+  result += frase[0]
+  return result
 }
+//console.log(concatName(['foguete', 'não', 'tem', 'ré']))
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties,) {
+  result = (wins*3) + (ties*1)
+  return result
   // seu código aqui
 }
+console.log(footballPoints(14, 8))
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(listNumber) {
+  let higherNumber = 0;
+  let countMaior = 0;
+  for(let indice in listNumber) {
+    if(higherNumber < listNumber[indice]){
+      higherNumber = listNumber[indice];
+    }
+  }
+//  return higherNumber
+  for(let indice2 in listNumber){
+    if(higherNumber === listNumber[indice2])
+    countMaior +=1;
+  }
+  return countMaior  
 }
+
+//console.log(highestCount([0, 0, 0]))
 
 // Desafio 7
 function catAndMouse() {
