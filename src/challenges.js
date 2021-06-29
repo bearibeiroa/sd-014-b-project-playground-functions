7884f705431e585b7c3f14e68eb61a2cf96f607b
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-06-29 14:41:46 -0400

    Commit Inicial do Projeto e Desafio 1 realizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  let result = false;
+  if (value1 === true && value2 === true){
+    result = true;
+  }
+  return result;
 }
-
 // Desafio 2
 function calcArea() {
   // seu código aqui
