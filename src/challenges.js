11f587be8a32a777dcc9e798d3d8e79627547cbf
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-06-29 17:22:54 -0300

    enviando o resquisito 06

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 === true && value2 === true){
+  if (value1 === true && value2 === true) {
     return true
   } else {
     return false
@@ -15,13 +15,13 @@ function calcArea(base, height) {
 
 
 // Desafio 3
-function splitSentence(){
-  
+function splitSentence() {
+
 }
 
 // Desafio 4
 function concatName(array) {
-  let calcu = array.length-1
+  let calcu = array.length - 1
   let phrase = (array[calcu] + ", " + array[0])
   return phrase
 }
@@ -30,45 +30,58 @@ function concatName(array) {
 // Desafio 5
 function footballPoints(wins, ties) {
   let winsPoints = wins * 3
-  let tiesPoints = ties 
+  let tiesPoints = ties
   let totalPoints = winsPoints + tiesPoints
   return totalPoints
 }
 
-console.log(footballPoints(14,8))
-
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+
+function highestCount(value){
+  let highNumber = 0;
+  let someNumber = 0;
+
+  for (let numberIndex = 0; numberIndex < value.length; numberIndex += 1) {
+
+
+    if (value[numberIndex] > highNumber) {
+      highNumber = value[numberIndex]
+    }
+
+    if (highNumber === value[numberIndex]) {
+      someNumber += 1
+    }
+  }
+  return someNumber
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+ // Desafio 7
+  function catAndMouse() {
+    // seu código aqui
+  }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+  // Desafio 8
+  function fizzBuzz() {
+    // seu código aqui
+  }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+  // Desafio 9
+  function encode() {
+    // seu código aqui
+  }
+  function decode() {
+    // seu código aqui
+  }
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-};
+  module.exports = {
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    highestCount,
+    splitSentence,
+  };
