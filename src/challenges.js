2295748134e5b4cf33e511b61cca2907d12e012e
lucas <mr.pine97@gmail.com>
2021-06-29 15:55:53 -0300

    Requesito #3

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  spl = str.split(' ')
+  return spl
 }
 
 // Desafio 4
