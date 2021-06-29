42254454c93198c6707e644abf560d7670ceb1df
bruholanda <brunaholandacunha7@gmail.com>
2021-06-29 16:36:13 -0300

    Exercício 4 (invertendo posições);

--- src/challenges.js
+++ src/challenges.js
@@ -15,7 +15,7 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  return array[0] + ", " + array[array.lenght - 1];
+  return array[array.lenght - 1] + ", " + array[0];
 }
 
 // Desafio 5
