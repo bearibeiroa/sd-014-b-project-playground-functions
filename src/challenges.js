ef448034585df7df458a72d381fb1858127a6bac
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-06-29 15:44:02 -0300

    Commit Inicial com requisito 1 já feito.

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  let result = false;
+  if (value1 === true && value2 === true) {
+    result = true;
+  }
+  return result;
 }
 
 // Desafio 2
