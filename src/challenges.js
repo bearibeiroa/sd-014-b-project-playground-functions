39f2d84bcc63821fb25ca10e1fd4d5e807846c32
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-06-29 15:22:58 -0400

    Realizado o desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,9 +13,11 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(word) {
+  let result = word.split(" ");
+  return result;
 }
+
 
 // Desafio 4
 function concatName() {
