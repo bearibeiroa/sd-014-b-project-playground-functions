004affda324ab4d5eb65800ff3b0019b696b787a
Thiago-siqueira <thiago_sique@hotmail.com>
2021-06-29 16:53:07 -0300

    quinto requesito do projeto playground functions

--- src/challenges.js
+++ src/challenges.js
@@ -36,9 +36,13 @@ console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + ties;
+  return points;
 }
+console.log(footballPoints(14, 8));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount() {
