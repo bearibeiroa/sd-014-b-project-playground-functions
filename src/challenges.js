020692b2abca51c7ee41af4514046e0c0431823d
Raphael Oliveira <raphael.caninde@gmail.com>
2021-06-29 18:20:47 -0300

    challenge #4 complete

--- src/challenges.js
+++ src/challenges.js
@@ -20,8 +20,9 @@ function splitSentence(str) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let namesArray = array[array.length - 1] + ", " + array[0];
+  return namesArray;
 }
 
 // Desafio 5
