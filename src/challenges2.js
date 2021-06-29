1f0ddbfa77cace4d15f0d36f6879fa6e49338539
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-06-29 20:54:33 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,19 +2,17 @@
 function techList(lista, name) {
   let sort = lista.sort();
   let nicolasLista = [];
-  if (lista.length === 0){
-    return 'Vazio';
-  } else {
-    for (index in sort) {
-      nicolasLista.push({'tech' : sort[index] , 'name' : name});
+  if (lista.length !== 0) {
+    for (let index in sort) {
+      nicolasLista.push({ 'tech': sort[index], 'name': name });
     }
-  }
-  
+    return 'Vazio';
+  }  
   return nicolasLista;
 }
 console.log(techList([], "Nicolas"));
 
-// Desafio 11
+/* Desafio 11
 function generatePhoneNumber(onzeNumeros) {
   // seu código aqui
 }
@@ -27,11 +25,11 @@ function triangleCheck() {
 // Desafio 13
 function hydrate() {
   // seu código aqui
-}
+} */
 
 module.exports = {
-  generatePhoneNumber,
+  // generatePhoneNumber,
   techList,
-  hydrate,
-  triangleCheck,
+  // hydrate,
+  // triangleCheck,
 };
