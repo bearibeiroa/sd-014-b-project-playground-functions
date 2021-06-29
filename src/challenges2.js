2f148e9baa0b63eccf5a37412ce1fdc602cd5b23
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-06-29 20:01:43 -0300

    Requisito #11

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,8 +12,64 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function conditions(numbers) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] < 0 || numbers[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }   
+}
+function repeatedNumber(numbers) {
+  let currentNumber = 0;
+  let qtdVezes = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    currentNumber = numbers[i];
+    for (let j = 0; j < numbers.length; j += 1) {
+      if (currentNumber === numbers[j]) {
+        qtdVezes += 1;
+      }
+    }
+    if (qtdVezes >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    qtdVezes = 0;
+  }
+}
+function checkLength(numbers) {
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+}
+function generatePhoneNumber(numbers) {
+  let condicaoTamanho = checkLength(numbers);
+  let condicaoNumeros = conditions(numbers);
+  let condicaoRepeticao = repeatedNumber(numbers);
+  if (condicaoTamanho !== undefined) {
+    return condicaoTamanho;
+  } else if (condicaoRepeticao !== undefined) {
+    return condicaoRepeticao;
+  } else if (condicaoNumeros !== undefined) {
+    return condicaoNumeros;
+  }
+  let phoneNumber = '(';
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (i < 2) {
+      phoneNumber += numbers[i];
+    }
+    if (phoneNumber.length === 3) {
+      phoneNumber += ') ';
+    }
+    if (i >= 2 && i < 7) {
+      phoneNumber += numbers[i];
+    }
+    if (phoneNumber.length === 10) {
+      phoneNumber += '-';
+    }
+    if (i >= 7) {
+      phoneNumber += numbers[i];
+    }
+  }
+  return phoneNumber;
 }
 
 // Desafio 12
