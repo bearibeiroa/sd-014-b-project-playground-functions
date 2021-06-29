80fb15dec961011d30838368a0fd8d93b3d91e40
Anderson Leite <andersonleite.dev@gmail.com>
2021-06-29 17:01:16 -0300

    Desafio #3 - Funçao splitSentence

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  let arrayStrings = phrase.split(' ');
+  return arrayStrings;
 }
 
 // Desafio 4
