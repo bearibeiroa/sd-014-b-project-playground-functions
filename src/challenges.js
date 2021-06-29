827ef099663032eb9ab7894e2c3b6ab84d9978e4
Rafael Veiga <rafaelveig@hotmail.com>
2021-06-29 16:39:41 -0300

    requisito #2

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,16 @@
 function compareTrue(valor1, valor2) {
   if(valor1 === true && valor2 === true){
     return true
-  } else { 
-    return false }
+  } else {
+    return false
+  }
 }
-
-
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base*height)/2
 }
-
+console.log(calcArea(10,50))
 // Desafio 3
 function splitSentence() {
   // seu código aqui
