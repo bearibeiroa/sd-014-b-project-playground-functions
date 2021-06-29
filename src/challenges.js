13255066e43c303bae11bc0353241809310c350a
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-29 19:23:04 -0300

    Exercicio 8 Complete

--- src/challenges.js
+++ src/challenges.js
@@ -51,44 +51,54 @@ function highestCount(numbers) {
 function catAndMouse(mouse, cat1, cat2) {
   let cat1distance = 0;
   let cat2distance = 0;
-  if (mouse > cat1 && mouse > cat2 ) {
-    for (let cat1index = cat1; cat1index < mouse; cat1index +=1) {
-        cat1distance += 1;
+   if (mouse > cat1 && mouse > cat2 ) {
+       for (let cat1index = cat1; cat1index < mouse; cat1index += 1) {
+           cat1distance += 1;
+       }
+       for (let cat2index = cat2; cat2index < mouse; cat2index += 1) {
+           cat2distance += 1;
+       }
+   } else if (cat1 < mouse && cat2 > mouse ) {
+       for (let cat1index =cat1; cat1index < mouse; cat1index += 1) {
+           cat1distance += 1;
+       }
+       for (let cat2index = mouse; cat2index < cat2; cat2index += 1) {
+           cat2distance += 1;
+       }
+   } else if (mouse < cat1 && mouse < cat2) {
+       for (let cat1index = mouse; cat1index < cat1; cat1index += 1) {
+           cat1distance += 1;
+       }
+       for (let cat2index = mouse; cat2index < cat2; cat2index += 1) {
+           cat2distance += 1;
+       }
+   }
+   if (cat1distance < cat2distance) {
+       return "cat1";
+    } else if (cat1distance > cat2distance) {
+        return "cat2";
+    } else {
+      return "os gatos trombam e o rato foge";
     }
-    for (let cat2index = cat2; cat2index < mouse; cat2index +=1) {
-        cat2distance += 1;
-    }
-  } else if (cat1 < mouse && cat2 > mouse ) {
-    for (let cat1index =cat1; cat1index < mouse; cat1index +=1) {
-        cat1distance += 1;
-    }
-    for (let cat2index = mouse; cat2index < cat2; cat2index +=1) {
-        cat2distance += 1;
-    }
-  } else if (mouse < cat1 && mouse < cat2) {
-    for (let cat1index = mouse; cat1index < cat1; cat1index +=1) {
-        cat1distance += 1;
-    }
-    for (let cat2index = mouse; cat2index < cat2; cat2index +=1) {
-        cat2distance += 1;
-    }
-}
-  if (cat1distance < cat2distance) {
-    return "cat1";
-} else if (cat1distance > cat2distance) {
-    return "cat2";
-} else {
-  return "os gatos trombam e o rato foge";
-}
-}
-
-catAndMouse(9, 5, 3);
-
+  }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let fizzBuzzResult = [];
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i] / 3 >= 1 && array[i] / 5 >= 1 && Number.isInteger(array[i] / 3) && Number.isInteger(array[i] / 5)) {
+            fizzBuzzResult.push("fizzBuzz");
+        } else if (array[i] / 3 >= 1 && Number.isInteger(array[i] / 3)){
+            fizzBuzzResult.push("fizz");
+        } else if (array[i] / 5 >= 1 && Number.isInteger(array[i] / 5)) {
+            fizzBuzzResult.push("buzz");
+        } else {
+            fizzBuzzResult.push("bug!");
+        }
+    }
+    return fizzBuzzResult;
 }
+
 
 // Desafio 9
 function encode() {
