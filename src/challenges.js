23a0885f4ef8eb4b95ba073026098f945dac19ff
Mateus Gambaro <magambaro@hotmail.com>
2021-06-29 19:30:39 -0300

    Requisito #9

--- src/challenges.js
+++ src/challenges.js
@@ -115,11 +115,16 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+  let novaString = string.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3).replace(/o/g, 4).replace(/u/g, 5);
+  return novaString;
+  
 }
-function decode() {
+function decode(string) {
   // seu código aqui
+  let novaString = string.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+  return novaString;
 }
 
 module.exports = {
