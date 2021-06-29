81bc9bdcda81410685612ea1ba5037d93f816245
Nataniel Santos <nathan.santos159@hotmail.com>
2021-06-29 16:52:11 -0300

    Requisto #6

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
-function compareTrue(value1,value2) {
-  if(value1 === true && value2 === true){
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
     return true;
   } else {
     return false;
@@ -10,7 +10,7 @@ function compareTrue(value1,value2) {
 // Desafio 2
 function calcArea(base, height) {
   let area = (base * height) / 2;
-  return area
+  return area;
 }
 
 // Desafio 3
@@ -23,20 +23,31 @@ function concatName(array) {
   const firstIndex = 0;
   let lastIndex = array.length - 1;
   let concatString = array[lastIndex] + ', ' + array[firstIndex];
-  
-  return concatString
-}
 
+  return concatString;
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let points = (wins * 3) + ties;
-  return points  
+  let points = wins * 3 + ties;
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let highestNumberIndex = 0;
+  let count = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > array[highestNumberIndex]) {
+      highestNumberIndex = i;
+    }
+  }
+  for (number of array) {
+    if (number === array[highestNumberIndex]) {
+      count++;
+    }
+  }
+  return count
 }
 
 // Desafio 7
