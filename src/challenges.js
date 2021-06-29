fbdc72d2d9b73744e4cfd43ef997270057c3fc22
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-29 15:52:12 -0300

    Conclusao Requisito #3

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(word) {
+  let separador = word.split(' ');
+  return separador;
 }
 
 // Desafio 4
