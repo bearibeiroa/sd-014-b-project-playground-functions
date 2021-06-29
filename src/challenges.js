e8c786f391562e44b6038356af42f25d98659abb
Nasser <nasseralmeida@gmail.com>
2021-06-29 18:43:51 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -14,7 +14,7 @@ return area;
 console.log(area);
 }
 
-// Desafio 3    // 
+// Desafio 3    // método split. referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
 function splitSentence(phrase) {
   let words = phrase.split(' ');
   return words;
@@ -22,7 +22,7 @@ function splitSentence(phrase) {
 
 // Desafio 4
 function concatName(arrayOfNames) {
-  return (arrayOfNames[arrayOfNames.length-1] + ", " + arrayOfNames[0]);
+  return (arrayOfNames[arrayOfNames.length - 1] + ", " + arrayOfNames[0]);
 }
 
 // Desafio 5
@@ -31,13 +31,42 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+let higherNum = numbers[0];
+let quant = 0;
+for (let counter = 0; counter < numbers.length; counter +=1){
+  if (numbers[counter] > higherNum){
+    higherNum = numbers[counter];
+  }
+}
+for (let counter = 0; counter < numbers.length; counter +=1){
+  if (higherNum === numbers[counter])
+  quant +=1;}
+
+return quant;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+
+  let distancia1 = cat1 - mouse;
+  let distancia2 = cat2 - mouse;
+
+  if (distancia1 < 0) {
+    distancia1 = distancia1 *-1;
+
+  }  if (distancia2 < 0) {
+    distancia2 = distancia2 *-1;
+
+  }  if (distancia1 === distancia2){
+
+      return 'os gatos trombam e o rato foge';
+
+  } else if (distancia1 > distancia2) {
+    return 'cat2';
+  } else {
+    return 'cat1';
+  }
 }
 
 // Desafio 8
