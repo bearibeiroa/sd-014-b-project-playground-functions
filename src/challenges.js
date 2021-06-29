95cea8ad1b2afa66177cbf682b768863929118e0
samsantosb <samsantosb@outlook.com>
2021-06-29 17:15:27 -0300

    algumas correçoes

--- src/challenges.js
+++ src/challenges.js
@@ -8,23 +8,26 @@ const calcArea = (base, height) => (base * height) / 2;
 const splitSentence = (str) => str.split(' ');
 
 // Desafio 4
-const concatName = (arr) => arr.join(', ');
+const concatName = (arr) => arr.join();
 
 // Desafio 5
-const footballPoints = (wim, tie) => win * 3 + tie;
+const footballPoints = (win, tie) => win * 3 + tie;
 
 // Desafio 6
 const highestCount = (arr) => {
   var max = Math.max(...arr);
   var countmax = arr.filter(max).length;
-
   return countmax;
 };
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+const catAndMouse = (mouse, cat, catt) => {
+  var dis1 = Math.abs(cat - mouse);
+  var dis2 = Math.abs(catt - mouse);
+  if (dis1 > dis2) return 'cat2';
+  if (dist1 > dist2) return 'cat1';
+  return 'os gatos trombam e o rato foge';
+};
 
 // Desafio 8
 function fizzBuzz() {
