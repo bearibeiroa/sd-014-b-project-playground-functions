5e327f10663400062ab95240e4b27c4772fb8560
Elithon Silva <elithonsilva@gmail.com>
2021-06-29 18:03:19 -0300

    Desafio-03

--- src/challenges.js
+++ src/challenges.js
@@ -15,9 +15,13 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string, espaco) {
   // seu código aqui
+  let array = string.split(' ',espaco);
+  return array;
 }
+
+console.log(splitSentence("go Trybe"));
 
 // Desafio 4
 function concatName() {
