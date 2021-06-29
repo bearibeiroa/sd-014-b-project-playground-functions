ad43dab2d332c8ee9c0a70237c67e60c2bf404fe
Davisralves <davialves324@gmail.com>
2021-06-29 19:44:09 -0300

    adicionando desafio 11 !

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,32 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if(array.length != 11) {  // verifica tamanho da array.
+    return "Array com tamanho incorreto.";
+  }
+  for(let numero of array) {  // verifica se existe um numero maior que 9 ou menor que 0 na array
+    if (numero > 9 || numero < 0) {
+      return "não é possível gerar um número de telefone com esses valores" ;
+    }
+  }
+  for (let index = 0; index <= 11; index += 1){  // verficia se existe um numero que se repete mais de 3 vezes na array.
+    let contador = 0;
+    for(let numero of array){
+      if (index === numero){
+        contador += 1;
+      }
+      if (contador >= 3){
+        return "não é possível gerar um número de telefone com esses valores" ;
+      }
+    }
+  }
+let ddd = '('+ array[0] + array[1] + ') ';
+array.splice(6, 0,'-');
+array.splice(0,2);
+array = array.join('');
+let numeroDeTelefone = (ddd + array); 
+return numeroDeTelefone;
 }
 
 // Desafio 12
