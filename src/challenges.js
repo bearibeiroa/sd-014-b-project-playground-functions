8d90df6ac4a9b95d77be391ddb9a8e4582c428b7
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-06-29 19:50:31 -0300

    Desafios 4 a 6 feitos

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,4 @@
 // Desafio 1
-
 function compareTrue(a, b) {
   if (a === true && b === true) {
    return true;
@@ -25,28 +24,40 @@ function splitSentence(frase) {
       word += frase[index];
     }   
   }
-    array.push(word);
-    word = '';
-  return array;
-  }
-
-  console.log(splitSentence('O Grêmio está horrível'));
-  
-
+ array.push(word);
+ word = '';
+ return array;
+}
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
+function concatName(array) {
+  return array[array.length -1] + ', ' + array[0];
+  }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins*3 + ties;
 }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+// Desafio 6 // 1 for para ver qual maior número e outro para o contador de repetição
+function highestCount(array) {
+ let posicao = 0;
+ let contador = 0;
+ let num = 0;
+ // Esse for busca o maior número do array e armazena na let num.
+  for (let index = 1; index < array.length; index += 1){
+    if (array[posicao] <= array[index]){
+      posicao = index;
+      num = array[posicao];
+    }
+  } 
+  // Esse for percorre o array contando quantas vezes o num repete.
+  for (let index2 = 0; index2 < array.length; index2 += 1){
+    if (array[index2] === num){
+      contador += 1;
+    }
+  }
+  return contador;
 }
 
 // Desafio 7
