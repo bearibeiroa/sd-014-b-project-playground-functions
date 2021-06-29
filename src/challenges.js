7ad8f0dee2799ee89f304d32519d8dbea6ef2d82
Gabriel <gabrielzegarram@gmail.com>
2021-06-29 15:38:17 -0300

    Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,9 @@ function compareTrue(valor1, valor2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, heigth) {
   // seu código aqui
+  return (base*heigth)/2;
 }
 
 // Desafio 3
