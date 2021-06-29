a71741096cc6843fb77c3386a38b1ab4e0fcb704
Bruno Trindade <brunotrndd@gmail.com>
2021-06-29 20:56:55 -0300

    Correção requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,30 +23,49 @@ console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"));
 
 // Desafio 11
 function generatePhoneNumber(numbers) {
-  let newNumbers = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0};  
-  for (let index = 0; index <= 9; index += 1) {
-    if (numbers.indexOf(index) === -1) {
-      newNumbers[numbers[index]] += 1; 
-    }
-  }  
+  let numbersIdx = numbers;
+  let newNumbers = findDuplicates(numbersIdx);
+
   for (let index = 0; index < 10; index += 1) {
-    if (newNumbers[index] >= 2) {
+    if (newNumbers[index] > 1) {
       return 'não é possível gerar um número de telefone com esses valores'
     }
   }
   if (numbers.length != 11) {
     return 'Array com tamanho incorreto.';
-  } else { 
+  } else {
     for (let number of numbers) {
       if (number < 0 || number > 9) {
         return 'não é possível gerar um número de telefone com esses valores'
-      } 
+      }
     }
   }
   return '(' + numbers[0] + numbers[1] + ') ' + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + '-' + numbers[7] + numbers[8] + numbers[9] + numbers[10];
 }
 
-console.log(generatePhoneNumber([1, 2, 8, 0, 5, 3, 7, 8, 9, 1, 8]));
+console.log(generatePhoneNumber([8, 2, 8, 0, 5, 3, 7, 3, 9, 6, 5]));
+
+function findDuplicates(arr) {
+  let sorted_arr = arr.slice().sort();
+  let results = [];
+  let newNumbers = { '0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0 };
+  for (let i = 0; i < sorted_arr.length - 1; i++) {
+    if (sorted_arr[i + 1] === sorted_arr[i]) {
+      results.push(sorted_arr[i]);
+    }
+  }
+  let index = 0;
+  while (index < 10) {
+  for (let result of results) {    
+      if (result === results[index]) {
+        newNumbers[result] += 1;
+      }      
+    }
+    index += 1;
+  }
+
+  return newNumbers;
+}
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {  
