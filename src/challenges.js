f9bd66c4ec0742f23ffac83392b3fcd21eb5b669
Raphael Oliveira <raphael.caninde@gmail.com>
2021-06-29 17:10:50 -0300

    challenge #2 complete

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,9 @@ function compareTrue(name1, name2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let result = (base * height) / 2;
+  return result;
 }
 
 // Desafio 3
