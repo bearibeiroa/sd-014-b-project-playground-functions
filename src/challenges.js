c74a1baa36908dd6afb1433fdf95692f30690a46
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-06-29 18:22:17 -0300

    Requisito #1

--- src/challenges.js
+++ src/challenges.js
@@ -32,8 +32,8 @@ function concatName(string) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return ((wins*3) + (ties*1))
 }
 
 // Desafio 6
