27c825cfb427f4c8be5e7a2cb991aff58038dcf8
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-29 17:51:26 -0300

    Requisito #6

--- src/challenges.js
+++ src/challenges.js
@@ -39,8 +39,20 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  let highestNumber = numbers[0];
+  let highestNumberCount = 0;
+  for (let iMaior = 0; iMaior < numbers.length; iMaior += 1) {
+      if (numbers[iMaior] > highestNumber) {
+          highestNumber = numbers[iMaior];
+      }    
+  }
+  for (let index = 0; index < numbers.length; index += 1) {
+      if (numbers[index] === highestNumber) {
+          highestNumberCount += 1;
+      }
+  }
+  return highestNumberCount;
 }
 
 // Desafio 7
