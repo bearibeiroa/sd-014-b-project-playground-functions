f1133c73546350d0cf90b8f317282e86cf8b1ad6
Leomar Linhares <leomar_linhares@hotmail.com>
2021-06-29 17:50:57 -0300

    Cumpre o desafio 9 :rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -74,11 +74,70 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let newString = '';
+
+  for (let index = 0; index < string.length; index++) {
+    switch (string[index]) {
+      case 'a':
+        newString += '1';
+        break;
+      
+      case 'e':
+        newString += '2';
+        break;
+      
+      case 'i':
+        newString += '3';
+        break;
+        
+      case 'o':
+        newString += '4';
+        break;
+          
+      case 'u':
+        newString += '5';
+        break;
+      
+      default:
+        newString += string[index]
+    };
+  }
+
+  return newString;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let newString = '';
+
+  for (let index = 0; index < string.length; index++) {
+    switch (string[index]) {
+      case '1':
+        newString += 'a';
+        break;
+      
+      case '2':
+        newString += 'e';
+        break;
+      
+      case '3':
+        newString += 'i';
+        break;
+        
+      case '4':
+        newString += 'o';
+        break;
+          
+      case '5':
+        newString += 'u';
+        break;
+      
+      default:
+        newString += string[index]
+    };
+  }
+
+  return newString;
 }
 
 module.exports = {
