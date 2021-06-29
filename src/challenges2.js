5defb8fda623243c4106102536f619147a4edb83
Kelner Roberto <kelner250@hotmail.com>
2021-06-29 20:41:48 -0300

    desafio 10 - techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, names) {
+  techs.sort()
+  let objeto = [];
+  if (techs.length === 0) {
+    return 'Vazio!'
+  }
+  for (let index = 0; index < techs.length; index += 1) {
+    let objetoItem = {
+      tech: techs[index],
+      name: names
+    }
+    objeto.push(objetoItem)
+  }
+  return objeto
 }
 
 // Desafio 11
