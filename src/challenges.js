49e644a52ac6e72f92b2c397bdceb5d0f29ceeb4
Abner Levi <abner@pop-os.localdomain>
2021-06-29 17:35:13 -0300

    Desafio #3

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
-  if (valor1 === true && valor2 === true){
+  if (valor1 === true && valor2 === true) {
     return true;
   } else {
     return false;
@@ -9,12 +9,12 @@ function compareTrue(valor1, valor2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height)/2;
+  return ( base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  return sentence.split(" ")
 }
 
 // Desafio 4
