7cd099213941b022dd50b3913d7d8bf80cc5e773
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-06-29 19:54:31 -0400

    Exercício 7 resolvido

--- src/challenges.js
+++ src/challenges.js
@@ -22,24 +22,45 @@ console.log(splitSentence('go Trybe'));
 
 // Desafio 4
 function concatName(arrayDeString) {
-  return arrayDeString[arrayDeString.length - 1] + " , " + arrayDeString[0];
+  return arrayDeString[arrayDeString.length - 1] + ", " + arrayDeString[0];
 }
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  wins = 3 * wins;
+  ties = 1 * ties;
+  return wins + ties;
 }
+console.log(footballPoints(3, 5));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numeros) {
+  let maiorNumero = 0;
+  let contNumero = 0;
+  for (let indice = 0; indice < numeros.length; indice += 1) {
+    if (numeros[indice] > maiorNumero) {
+      maiorNumero = numeros[indice];
+      contNumero += 1;
+    }
+  }
+  return contNumero;
 }
+console.log(highestCount([2, 10, 6, 10, 10, 6, 6, 10, 10]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  distanciaCat1 = cat1 - mouse;
+  distanciaCat2 = cat2 - mouse;
+  if (distanciaCat1 > distanciaCat2) {
+    return "cat2";
+  } else if (distanciaCat2 > distanciaCat1) {
+    return "cat1";
+  } else {
+    return "os gatos trombam e o rato foge";
+  }
 }
+console.log(catAndMouse(10, 20, 20));
 
 // Desafio 8
 function fizzBuzz() {
@@ -50,6 +71,8 @@ function fizzBuzz() {
 function encode() {
   // seu código aqui
 }
+
+// Desafio 10
 function decode() {
   // seu código aqui
 }
