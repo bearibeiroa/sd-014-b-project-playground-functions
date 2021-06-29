0e2275aaef890345f9fc73b7ad4bef80b6b376c2
Fran Ribeiro <ribeirofrv@gmail.com>
2021-06-29 16:03:20 -0300

    Iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,9 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valueOne, valueTwo) {
+  if (valueOne === valueTwo) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
