5ea841222036b130b7c8a500c424ea3871c01581
GustavoDamm <gustavo.damm15@hotmail.com>
2021-06-29 15:47:24 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -12,8 +12,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  let fraseF = phrase.split(' ');
+  return fraseF;
 }
 
 // Desafio 4
