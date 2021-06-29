42441836d9bd47df8c6fa30c5c5c318ac336dc51
Vanim <vanimouzad123@gmail.com>
2021-06-29 16:21:37 -0300

    Requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -29,8 +29,29 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let result = 0;
+
+  for (let i = 1; i <= wins; i += 1) {
+
+    if (wins === 0) {
+      break;
+    }
+
+    result += 3;
+
+  }
+
+  for (let i = 1; i <= ties; i += 1) {
+
+    if (ties === 0) {
+      break;
+    }
+
+    result += 1;
+  }
+
+  return result;
 }
 
 // Desafio 6
