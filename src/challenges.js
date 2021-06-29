dfa0df665ee46782416ab60e69fad06135911fd0
perchespierri <perchespierri@gmail.com>
2021-06-29 18:22:45 -0300

    Exercicios do 1 ao 8 prontos

--- src/challenges.js
+++ src/challenges.js
@@ -1,22 +1,21 @@
 // Desafio 1
 function compareTrue(a, b) {
-  if (a == true && b == true){
+  if (a === true && b === true) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height)/2;
+  let area = (base * height) / 2;
   return area;
 }
 
 // Desafio 3
 function splitSentence(str) {
-  let split = str.split(" ");
+  let split = str.split(' ');
   return split;
 }
 
@@ -24,7 +23,7 @@ function splitSentence(str) {
 function concatName(vetor) {
   let posicaoFinal = vetor[(vetor.length - 1)];
   let posicaoInicial = vetor[0];
-  let concatenated = posicaoFinal.concat(", " + posicaoInicial);
+  let concatenated = posicaoFinal.concat(', ', + posicaoInicial);
   return concatenated; 
 }
 
@@ -35,26 +34,56 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numeros) {
+  let maior = -3;
+  for (i of numeros){
+    if (i >= maior){
+      maior = i;
+    }
+  }
+  let cont = 0;
+  for (j of numeros){
+    if (j === maior){
+      cont++;
+    }
+  }
+  return cont;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dmc1 = Math.abs(mouse - cat1);
+  let dmc2 = Math.abs(mouse - cat2);
+  if (dmc1 > dmc2){
+    return 'cat2';
+  } else if (dmc1 < dmc2){
+    return 'cat1';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let vetor = [];
+  for (i of array){
+    if (i%3 === 0 && i%5 === 0){
+      vetor.push('fizzBuzz');
+    } else if (i%3 === 0 && i%5 !== 0){
+      vetor.push('fizz');
+    } else if (i%3 !== 0 && i%5 === 0){
+      vetor.push('buzz');
+    } else {
+      vetor.push('bug!');
+    }
+  }
+  return vetor;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(array) {
 }
-function decode() {
-  // seu código aqui
+function decode(array) {
 }
 
 module.exports = {
