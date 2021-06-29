735c688d0163730f79095820b62fc3e458c60209
bruholanda <brunaholandacunha7@gmail.com>
2021-06-29 17:34:11 -0300

    alteração parenteses

--- src/challenges.js
+++ src/challenges.js
@@ -15,7 +15,8 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  return (array[array.length - 1] + ', ' + array[0]);
+  var concat = (array[array.length - 1] + ', ' + array[0]);
+  return concat;
 }
 
 // Desafio 5
