1e766d43e4451d690250bb66b3f516e58f3d9ede
brenoMatias <matiasbreno19@gmail.com>
2021-06-29 18:10:28 -0300

    Desafio 10

--- src/challenges.js
+++ src/challenges.js
@@ -110,8 +110,16 @@ function decode(string2) {
 }
 
 // Desafio 10
-function spliteSentence ()
-
+function splitSentence (techIn, nameIn) {
+  let objectOut = [];
+  if (techIn.length == 0) {
+    return 'Vazio';      
+  }
+  techIn.sort();
+  for (let i = 0; i < techIn.length; i += 1) {
+    objectOut.push({tech: techIn[i], name: nameIn });
+  } return objectOut;
+}
 
 module.exports = {
   calcArea,
