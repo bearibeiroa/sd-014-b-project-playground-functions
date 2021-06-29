adb65b6ea55e14e7c9f1ba767ddb6ea1c0a00d0d
Cecilia Cacais <ceci.cacais@gmail.com>
2021-06-29 16:01:54 -0300

    Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(param1, param2) {
-  if (param1 === true && param2 === true){
+  if (param1 === true && param2 === true) {
     return true;
   } else {
     return false;
@@ -8,8 +8,8 @@ function compareTrue(param1, param2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base*height)/2
 }
 
 // Desafio 3
