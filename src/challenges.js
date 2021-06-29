08dc81ebe2ead341793d375ab14b2b7c9455cad7
Nataniel Santos <nathan.santos159@hotmail.com>
2021-06-29 18:56:39 -0300

    Requisito #9

--- src/challenges.js
+++ src/challenges.js
@@ -45,49 +45,98 @@ function highestCount(array) {
       count++;
     }
   }
-  return count
+  return count;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let cat1Distance = cat1 - mouse;
-  cat1Distance = Math.abs(cat1Distance)
+  cat1Distance = Math.abs(cat1Distance);
   let cat2Distance = cat2 - mouse;
-  cat2Distance = Math.abs(cat2Distance)
+  cat2Distance = Math.abs(cat2Distance);
 
-  if (cat1Distance > cat2Distance){
-    return "cat2"
+  if (cat1Distance > cat2Distance) {
+    return 'cat2';
   } else if (cat1Distance < cat2Distance) {
-    return "cat1"
-  } else if(cat1Distance === cat2Distance){
-    return "os gatos trombam e o rato foge"
+    return 'cat1';
+  } else if (cat1Distance === cat2Distance) {
+    return 'os gatos trombam e o rato foge';
   }
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   let newArray = [];
-  for(i=0;i<array.length;i+=1){
-    if(array[i] % 3 === 0 && array[i] % 5 === 0){
-      newArray.push('fizzBuzz')
-    }else if(array[i] % 3 === 0){
-      newArray.push('fizz')
-    } else if(array[i] % 5 === 0){
-      newArray.push('buzz')
+  for (i = 0; i < array.length; i += 1) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      newArray.push('fizzBuzz');
+    } else if (array[i] % 3 === 0) {
+      newArray.push('fizz');
+    } else if (array[i] % 5 === 0) {
+      newArray.push('buzz');
     } else {
-      newArray.push('bug!')
+      newArray.push('bug!');
     }
   }
 
-  return newArray
+  return newArray;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let splittedString = string.split('');
+  let newString = '';
+
+  for (let letter of splittedString) {
+    switch (letter) {
+      case 'a':
+        newString += '1';
+        break;
+      case 'e':
+        newString += '2';
+        break;
+      case 'i':
+        newString += '3';
+        break;
+      case 'o':
+        newString += '4';
+        break;
+      case 'u':
+        newString += '5';
+        break;
+      default:
+        newString += letter;
+    }
+  }
+  return newString;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let splittedString = string.split('');
+  let newString = '';
+
+  for (let letter of splittedString) {
+    switch (letter) {
+      case '1':
+        newString += 'a';
+        break;
+      case '2':
+        newString += 'e';
+        break;
+      case '3':
+        newString += 'i';
+        break;
+      case '4':
+        newString += 'o';
+        break;
+      case '5':
+        newString += 'u';
+        break;
+      default:
+        newString += letter;
+    }
+  }
+  return newString;
 }
 
 module.exports = {
