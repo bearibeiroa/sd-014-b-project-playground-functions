32d6d7af084385af31fd1eaedc155e8e6118c94b
Lombeira <glomenha@gmail.com>
2021-06-29 16:37:31 -0300

    Requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -17,8 +17,8 @@ function splitSentence(str) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[0] + ', ' + array[array.length - 1] + '.';
 }
 
 // Desafio 5
