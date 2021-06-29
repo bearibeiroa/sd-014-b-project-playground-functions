610ba9f1357d76d39de53b8947c123efbf9af62d
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-06-29 19:41:55 -0400

    Relizado o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,12 +15,57 @@ function techList(tecnologies, name) {
   }
   return result;
 }
-techList([],'Lucas');
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let telephoneNumber = "";
+  let repetManyTimes = checkRepetedTimes(numbers);
+  let canMakeNumber = false;
+  let result = "";
+  for (let i = 2; i < numbers.length; i += 1){
+    let number = numbers[i];
+    if (numbers.length > 11 || numbers.length < 11){
+      canMakeNumber = false;
+      result = "Array com tamanho incorreto.";
+      break;
+    } else if (number < 0 || number > 9 || repetManyTimes === true){
+      canMakeNumber = false
+      result = "não é possível gerar um número de telefone com esses valores";
+      break;
+    } else if (i === 7){
+      canMakeNumber = true;
+      telephoneNumber += "-" + number.toString();
+    } else{
+      canMakeNumber = true;
+      telephoneNumber += number.toString();
+    }
+  }
+  if (canMakeNumber){
+    let ddd = numbers[0].toString() + numbers[1].toString();
+    result = "("+ddd+")"+" " + telephoneNumber;
+  } 
+  console.log(result);
+  return result;
 }
+
+function checkRepetedTimes(numbers){
+  let checkedNumbers = {};
+  let repeted3Times = false;
+  for (i of numbers){
+    if (checkedNumbers[i]){
+      checkedNumbers[i] += 1;
+    } else{
+      checkedNumbers[i] = 1;
+    }
+    if (checkedNumbers[i] >= 3){
+      repeted3Times = true;
+    }
+  }
+  return repeted3Times;
+}
+
+generatePhoneNumber([0,1,6]);
+
 
 // Desafio 12
 function triangleCheck() {
