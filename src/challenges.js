31582744db650bf838f22062fd8c69f484837177
João Reis <joaoreisjpk@gmail.com>
2021-06-29 16:27:48 -0300

    até a 9

--- src/challenges.js
+++ src/challenges.js
@@ -1,48 +1,129 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(valorA, valorB) {
+  return valorA && valorB;
   // seu código aqui
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, heigth) {
+  return ((base * heigth) / 2);
   // seu código aqui
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(str) {
+  return str.split(" ");
   // seu código aqui
 }
 
 // Desafio 4
-function concatName() {
+function concatName(str) {
+  return (str[str.length -1] + ', ' + str[0]);
   // seu código aqui
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
+  return (wins * 3 + ties * 1);
   // seu código aqui
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(int) {
+  let num = 0; let cont = 0;
+  for (let index = 0; index < int.length; index += 1) {
+    if (int[index] > num) {
+      num = int[index];
+    }
+  }
+  for (let index = 0; index < int.length; index += 1) {
+    if (int[index] === num) {
+      cont += 1;
+    }
+  }
+  return cont;
   // seu código aqui
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
+  let distUm = (mouse - cat1); let distDois = (mouse - cat2);
+  if (distUm < 0) {
+    distUm = distUm * (-1);
+  }
+  if (distDois < 0) {
+    distDois = distDois * (-1);
+  }
+  if (distUm < distDois) {
+    return 'cat1';
+  }
+  if (distDois < distUm) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
+
   // seu código aqui
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
+  let answer = [];
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] % 3 === 0) {
+      if (array[index] % 5 === 0) {
+        answer.push('fizzbuzz');
+      }
+      else {
+        answer.push('fizz');
+      }
+    }
+    else if (array[index] % 5 === 0) {
+      answer.push('buzz');
+    }
+    else {
+      answer.push('bug!');
+    }
+  } return answer;
   // seu código aqui
 }
 
 // Desafio 9
-function encode() {
+function encode() {    
+  let newText = '';
+  for(let l of text) {
+    if(l === "a") {
+        newText += "1";
+    } else if (l === "e") {
+        newText += "2";
+    } else if (l==="i") {
+       newText += "3";
+    } else if (l === "o") {
+        newText += "4";
+    } else if (l ==="u") {
+        newText += "5";
+    } else {
+        newText += l;
+    }
+} 
   // seu código aqui
 }
 function decode() {
+  let newText = '';
+  for(let l of text) {
+      if(l === "1") {
+          newText += "a";
+      } else if (l === "2") {
+          newText += "3";
+      } else if (l==="3") {
+         newText += "i";
+      } else if (l === "4") {
+          newText += "o";
+      } else if (l ==="5") {
+          newText += "u";
+      } else {
+          newText += l;
+      }
+  } 
   // seu código aqui
 }
 
