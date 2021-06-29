8bfa174a06889373bad0074aecdd1b85ddfe23af
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-29 16:06:18 -0300

    [Desafio 3] Completo

--- src/challenges.js
+++ src/challenges.js
@@ -15,10 +15,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
-
 // Desafio 4
 function concatName() {
   // seu código aqui
