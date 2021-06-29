3156cd389bd298f5eeff289b305de28eb2e7ca08
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-06-29 18:44:46 -0300

    Décimo requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
-function techList() {
+function techList(arrayTecnologias,name) {
   // seu código aqui
+  let arrayOrdenado = arrayTecnologias.sort();
+  let resultado = [];
+  for (let index = 0; index < arrayOrdenado.length; index += 1) {
+    let object = {};
+    object = {
+      tech: arrayOrdenado[index],
+      name: name 
+    }
+    resultado.push(object)
+  }
+  if (arrayOrdenado.length > 0){
+    return resultado
+  } else {
+    return "Vazio!"
+  }
+  
 }
 
 // Desafio 11
