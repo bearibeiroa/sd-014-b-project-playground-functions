a4ec7b273045f076c0eb917b354e65ccfd4730e8
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-06-29 19:57:33 -0300

    Décimo-primeiro requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,19 +9,57 @@ function techList(arrayTecnologias,name) {
       tech: arrayOrdenado[index],
       name: name 
     }
-    resultado.push(object)
+    resultado.push(object);
   }
   if (arrayOrdenado.length > 0){
-    return resultado
+    return resultado;
   } else {
-    return "Vazio!"
+    return "Vazio!";
   }
   
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeroDeTelefone) {
+  // seu código aqui 
+  let maiorNumero = numeroDeTelefone[0];
+  for (let indice in numeroDeTelefone) {
+    if (maiorNumero < numeroDeTelefone[indice]) {
+      maiorNumero = numeroDeTelefone[indice];      }
+  }
+
+  let menorNumero = numeroDeTelefone[0];
+  for (let indice in numeroDeTelefone) {
+    if (menorNumero > numeroDeTelefone[indice]) {
+      menorNumero = numeroDeTelefone[indice];      }
+  }
+  
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+  for (let index in numeroDeTelefone) {
+    let verificaNumero = numeroDeTelefone[index];
+    for (let index2 in numeroDeTelefone) {
+      if (verificaNumero === numeroDeTelefone[index2]) {
+        contNumero += 1;
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+
+  if (numeroDeTelefone.length !== 11) {
+      return 'Array com tamanho incorreto.'
+  } else if (maiorNumero > 9 || menorNumero < 0 || contRepetido >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores'
+  } else {
+      let numeroEstruturado = `(${numeroDeTelefone[0]}${numeroDeTelefone[1]}) ${numeroDeTelefone[2]}${numeroDeTelefone[3]}${numeroDeTelefone[4]}${numeroDeTelefone[5]}${numeroDeTelefone[6]}-${numeroDeTelefone[7]}${numeroDeTelefone[8]}${numeroDeTelefone[9]}${numeroDeTelefone[10]}`;
+      return numeroEstruturado;
+  }
+
 }
 
 // Desafio 12
