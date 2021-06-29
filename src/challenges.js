5642b62c930d53d2b0fdfa13774ae8ef453fbd1b
icaroemanuel <icaro.emanuel@outlook.com>
2021-06-29 16:40:53 -0300

    challenge#2

--- src/challenges.js
+++ src/challenges.js
@@ -6,8 +6,10 @@ function compareTrue(valorUm, valorDois) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let result = (base * height) / 2;
+  return result;
 }
 
 // Desafio 3
