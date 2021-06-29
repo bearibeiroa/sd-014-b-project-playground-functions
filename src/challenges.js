c1e09f75358ba20c613f50fd4150c1afee6810b9
eli <leonardohcds@gmail.com>
2021-06-29 18:20:01 -0300

    finished requirements #4 #6 and #7

--- src/challenges.js
+++ src/challenges.js
@@ -7,10 +7,12 @@ function compareTrue(bool1, bool2) {
   }
 }
 
+
 // Desafio 2
 function calcArea(base, height) {
   return (base * height) / 2;
 }
+
 
 // Desafio 3
 function splitSentence(phrase) {
@@ -34,26 +36,65 @@ function splitSentence(phrase) {
 
 // Desafio 4
 function concatName(array) {
-  let lastThenFirstName = '';
-  for(let i = array.length; i < array.length; i -= 1){
-    lastThenFirstName = array.length[i] + array[index];
-    // TO DO
-  }
+  return array[array.length - 1] + ', ' + array[0];
 }
+
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return (wins * 3) + ties;
 }
 
+
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) { // [9, 1, 2, 3, 9, 5, 7]
+  let highestNum = 0;
+  let highestNumFrequency = 0;
+  let currentNumFrequency = 0;
+  let currentNum = 0;
+
+  for(let i = 0; i < numbers.length; i += 1){
+    currentNum = numbers[i]; //9
+    for(let j = 0; j < numbers.length; j += 1){
+      if (currentNum == numbers[j]){
+        currentNumFrequency += 1;
+      }
+    } // 2
+    if ( i == 0 || currentNum > highestNum){
+      highestNum = currentNum;
+      highestNumFrequency = currentNumFrequency;
+    }
+    currentNumFrequency = 0;
+  }
+  return highestNumFrequency;
 }
 
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distance1 = 0;
+  let distance2 = 0;
+
+  if(cat1 > mouse){
+    distance1 = cat1 - mouse;
+  }else{
+    distance1 = mouse - cat1;
+  }
+
+  if(cat2 > mouse){
+    distance2 = cat2 - mouse;
+  }else{
+    distance2 = mouse - cat2;
+  }
+
+  if(distance1 < distance2){
+    return 'cat1';
+  }else if(distance1 > distance2){
+    return 'cat2';
+  }else{
+    return "os gatos trombam e o rato foge";
+  }
+
 }
 
 // Desafio 8
