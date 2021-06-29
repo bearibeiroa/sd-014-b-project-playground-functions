81c6442a6631dbb347046d5cffdfd7c1d75ba558
Kelner Roberto <kelner250@hotmail.com>
2021-06-29 15:42:30 -0300

    Desafio no. 2 - área do triângulo

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,9 @@ function compareTrue(valor1, valor2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let result = base*height/2;
+  return result;
 }
 
 // Desafio 3
