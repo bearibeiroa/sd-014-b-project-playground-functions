6aebf6c144909194b2ab90f3a13ee70b9f636590
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-06-29 16:01:43 -0300

    Requisito 2 - Crie uma função que calcule a área de um triângulo

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,9 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let areaValue = (base * height) / 2;
+  return areaValue;
 }
 
 // Desafio 3
