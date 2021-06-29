e413fb1affe5cf2fbc939af7745a3a21a9a2b0cb
Guilherme <guimfontes7@gmail.com>
2021-06-29 18:41:02 -0300

    Caça ao Rato

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,5 @@
 // Desafio 1
-function compareTrue(a,b) {
+function compareTrue(a, b) {
   let compare = true;
   if (a && b === true) {
     compare = true;
@@ -9,19 +9,19 @@ function compareTrue(a,b) {
   return compare;
   // seu código aqui
 }
-console.log(compareTrue(true,true));
-console.log(compareTrue(false,true));
-console.log(compareTrue(true,false));
-console.log(compareTrue(false,false));
+console.log(compareTrue(true, true));
+console.log(compareTrue(false, true));
+console.log(compareTrue(true, false));
+console.log(compareTrue(false, false));
 
 // Desafio 2
-function calcArea(base,height) {
+function calcArea(base, height) {
   let area = (base * height)/2;
   return area;
 }
-console.log(calcArea(10,50));
-console.log(calcArea(5,2));
-console.log(calcArea(51,1))
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence(string) {
@@ -35,35 +35,63 @@ console.log(splitSentence('foguete'));
 // Desafio 4
 function concatName(array) {
   let sum = [];
-  for (i = 0; i < array.length; i++) {
-  sum = sum += array[i];
-}
-return sum;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum += array[i];
+  }
+  return sum;
 }
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
-function footballPoints(wins,ties) {
+function footballPoints(wins, ties) {
   let pontos = (wins * 3) + ties;  
   return pontos;
 }
-console.log(footballPoints(14, 8))
-console.log(footballPoints(1, 2))
-console.log(footballPoints(0, 0))
+console.log(footballPoints(14, 8));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount(array) {
   let contNumero = 0;
   let contRepetido = 0;
-  
+  let indexNumeroRepetido = 0;
+    for (let i in array) {
+        let verificaNumero = array[i];
+        for (let j in array) {
+            if (verificaNumero === array[j]) {
+                contNumero += 1;
+            }
+        }
+        if (contNumero > contRepetido) {
+            contRepetido = contNumero;
+            indexNumeroRepetido = i;
+        }
+        contNumero = 0;
+    }
+    return array[indexNumeroRepetido];
 }
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let maisPerto;
+  if((cat1 - mouse) < (cat2 - mouse)) {
+    maisPerto = "cat1";
+  } else if (cat1 === cat2) {
+    maisPerto = "os gatos trombam e o rato foge";
+  } else {
+    maisPerto = "cat2";
+  }
+  return maisPerto;
 }
+console.log(catAndMouse(0, 3, 2));
+console.log(catAndMouse(0, 6, 12));
+console.log(catAndMouse(0, 2, 2));
 
 // Desafio 8
 function fizzBuzz() {
@@ -71,9 +99,23 @@ function fizzBuzz() {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let vogal = ['a', 'e', 'i', 'o', 'u'];
+  let valor = ['1', '2', '3', '4', '5']
+  let letra = string.split('');
+  for (let i in letra) {
+    letra[i] === vogal[i]
+  }
+  return res;
+/*
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+*/
 }
+console.log(encode("hi there!"))
 function decode() {
   // seu código aqui
 }
