2bce08436d8153be0b41bd050a1aef9eedd4762b
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-06-29 18:44:24 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -74,9 +74,29 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse,cat1,cat2) {
+  
+  let distancia1 = cat1 - mouse;
+  let distancia2 = cat2 - mouse;
+  
+  if (distancia1 < 0){
+    distancia1 = distancia1 *-1;
+
+  }if  (distancia2 < 0){
+    distancia2 = distancia2 *-1;
+  }
+  
+  if(distancia1 === distancia2){
+    
+    return 'os gatos trombam e o rato foge';
+  }else if (distancia1 > distancia2){
+    return 'cat2';
+  }else{
+     return 'cat1';
+  }
+  ;
 }
+
 
 // Desafio 8
 function fizzBuzz() {
