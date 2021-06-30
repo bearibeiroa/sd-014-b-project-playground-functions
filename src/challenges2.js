d85986860e574cd001fb81d731f290ed16bb11fc
jarana-2020 <julioaranabr@yahoo.com.br>
2021-06-29 23:57:43 -0300

    Requisito #10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  tech.sort();
+  function createObjects(tech, name) {
+    this.tech = tech;
+    this.name = name;
+  }
+  newList = [];
+  for (let index = 0; index < tech.length; index += 1) {
+    newList[index] = new createObjects(tech[index], name);
+  }
+  if (tech.length === 0) {
+    return 'Vazio!';
+  } else {
+    return newList;
+  }
+
 }
+techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas");
 
 // Desafio 11
 function generatePhoneNumber() {
