23372018ccac34cc6797d0e4366265b538413b50
brenoMatias <matiasbreno19@gmail.com>
2021-06-29 22:56:51 -0300

    Desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -26,8 +26,17 @@ function triangleCheck(lineA, lineB, lineC) {
   return true;
 }
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(drink) {
+  let allDrinks = drink.match(/\d/g); // The match() method retrieves the result of matching a string against a regular expression.
+  // Use the regular expression \d+, which means any digit from 0 to 9 (\d) repeated one or more times (+). The qualifier g will make the search global 
+  let water = 0; // quantidade de água inicial
+  for (let index = 0; index < allDrinks.length; index += 1) { // pecorre a string das bebidas
+    water += Number(allDrinks[index]); // The Number() function converts the object argument to a number that represents the object's value.
+  } 
+  if(water === 1) {
+    return `${water} copo de água`;// Template Literals '${} 'também permite interpolar valores em um texto
+  } 
+  return `${water} copos de água`;
 }
 
 module.exports = {
