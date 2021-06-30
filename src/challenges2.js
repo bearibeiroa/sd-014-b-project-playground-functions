4779d6c265c8dcbbcddc2cc89e0414a380fed223
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-06-29 21:43:03 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,10 +20,36 @@ function techList(arrayTech, stringName) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(telephoneNumber) {
   // seu código aqui
+  let retorno = '';
+  //Confere se o número do telefone é válido
+  if (telephoneNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index of telephoneNumber){
+    let contador = 0;
+    for (let indice = 0; indice < telephoneNumber.length; indice += 1) {
+      if (telephoneNumber[index] === telephoneNumber[indice]) {
+        contador += 1;
+      }
+    }
+    if (index < 0 || index > 9 || contador >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } 
+  }
+  //Coloca o número de telefone no formato pedido
+  for (let index = 0; index < telephoneNumber.length; index += 1) {
+    if (index === 0) {
+      retorno += `(${telephoneNumber[index]}`;
+    } else if (index === 1) {
+      retorno += `${telephoneNumber[index]}) `;
+    } else if (index === 6) {
+      retorno += `${telephoneNumber[index]}-`;
+    } else retorno += telephoneNumber[index];
+  }
+  return retorno;
 }
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
