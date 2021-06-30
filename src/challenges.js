799a2fa5707cfeb4bdbcfa43083759242ff1ae0a
Danilo dos Santos <danilo.santossouza@outlook.com>
2021-06-30 11:31:15 -0300

    Requisito 8 resolvido

--- src/challenges.js
+++ src/challenges.js
@@ -69,23 +69,27 @@ function catAndMouse(mouse, cat1, cat2) {
   }
 }
 
+
+
 // Desafio 8
 function fizzBuzz(numeros) {
   // seu código aqui
   result = [];
   for (let index = 0; index < numeros.length; index += 1){
-    if (numeros[index] % 3 == 0){
-      result.push("fizz");
-    }else if (numeros[index] % 5 == 0) {
-      result.push("buzz");
-    }else if (numeros[index] % 3 == 0 && numeros[index] % 5 == 0) {
+    if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0){
       result.push("fizzBuzz");
+    }else if (numeros[index] % 3 === 0) {
+      result.push("fizz");
+    }else if (numeros[index] % 5 === 0 ) {
+      result.push("buzz");
     }else {
       result.push("bug!");
     }
   }
   return result;
 }
+fizzBuzz([2, 15, 7, 9, 45])
+console.log(result);
 
 // Desafio 9
 function encode() {
