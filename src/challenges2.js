ae6726b14cc9cc462f311d056f68c0455c34f1de
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-06-29 22:42:08 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,35 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function sortByTech(listOfTechs) {
+  return listOfTechs.sort(function (a, b) {
+    if (a.tech > b.tech) {
+      return 1;
+    }
+    if (a.tech < b.tech) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
+/** 
+ * Para criar uma função para ordenar um array de objetos de acordo com o
+ * valor de uma de suas propriedades, pesquisei sobre o método array.sort()
+ * Link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+*/
+
+function techList(tech, name) {
+  let listOfTechs = [];
+  for (let index = 0; index < tech.length; index += 1) {
+    let newTech = {
+      tech: tech[index],
+      name,
+    };
+    listOfTechs.push(newTech);
+  }
+  if (listOfTechs.length === 0) {
+    return 'Vazio!';
+  }
+  return sortByTech(listOfTechs);
 }
 
 // Desafio 11
