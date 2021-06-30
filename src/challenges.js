1c3ef6fbabf351aff7fb1a911677008c17489815
miguel-alexandre <sauloj@protonmail.com>
2021-06-30 16:44:43 -0300

    6 requisito consertado 8

--- src/challenges.js
+++ src/challenges.js
@@ -46,13 +46,13 @@ function footballPoints(wins, ties) {
 function highestCount(numbers) {
   // seu código aqui
   numbers.sort((e, f) => f - e);
-  if (numbers.length > 0) {
+  if (numbers.length > 1) {
     let biggest = 0;
     let counts = 0;
     for (let i = 0; i < numbers.length; i += 1) {
       if (numbers[i] >= biggest) {
         biggest = numbers[i];
-        counts += 1;
+        counts++;
       }
     }
     return counts;
