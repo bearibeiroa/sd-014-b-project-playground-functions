3d83265b0d96a034079f7d6d8a5f3779c2f0bc36
Cezar <cezarguimaraes@gmail.com>
2021-06-29 21:26:42 -0300

    encodeDecode done

--- src/challenges.js
+++ src/challenges.js
@@ -87,14 +87,45 @@ function fizzBuzz(ary) {
   }
   return testfizzbuzz;
 }
-console.log(fizzBuzz([9, 25]));
+
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(strg){
+  let newstrg = "";
+  for (index3 = 0; index3 < strg.length; index3 +=1){
+    if (strg[index3] === "a"){
+      newstrg = newstrg + "1";
+    } else if (strg[index3] === "e"){
+      newstrg = newstrg + "2";
+    } else if (strg[index3] === "i"){
+      newstrg = newstrg + "3";
+    } else if (strg[index3] === "o"){
+      newstrg = newstrg + "4";
+    } else if (strg[index3] === "u"){
+      newstrg = newstrg + "5";
+    } else {newstrg = newstrg + strg[index3];
+    }
+  }
+  return newstrg;
 }
-function decode() {
-  // seu código aqui
+
+function decode(strg2) {
+  let newstrg2 = "";
+  for (index4 = 0; index4 < strg2.length; index4 +=1){
+    if (strg2[index4] === "1"){
+      newstrg2 = newstrg2 + "a";
+    } else if (strg2[index4] === "2"){
+      newstrg2 = newstrg2 + "e";
+    } else if (strg2[index4] === "3"){
+      newstrg2 = newstrg2+ "i";
+    } else if (strg2[index4] === "4"){
+      newstrg2 = newstrg2 + "o";
+    } else if (strg2[index4] === "5"){
+      newstrg2 = newstrg2 + "u";
+    } else {newstrg2 = newstrg2 + strg2[index4];
+    }
+  }
+  return newstrg2;
 }
 
 module.exports = {
---
+++ src/rascunho.js
@@ -0,0 +1,7 @@
+let newstring = "";
+let test = "cara de cu";
+
+for (index = 0; index<test.length; index++){
+    newstring = newstring + test[index];
+}
+console.log(newstring);
