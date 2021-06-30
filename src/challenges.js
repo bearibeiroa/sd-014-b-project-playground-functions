60ad6ec040f763634d05db8b8b6d8f901096b1af
Thyara <thyaranonato@outlook.com>
2021-06-30 20:25:19 -0300

    Adiciona o Desafio #9

--- src/challenges.js
+++ src/challenges.js
@@ -10,7 +10,7 @@ function calcArea(base, height) {
 
 // Desafio 3
 // Para este desafio foi pesquisado sobre o método split no seguinte site:
-// https://www.w3schools.com/jsref/jsref_split.asp
+// Source: https://www.w3schools.com/jsref/jsref_split.asp
 function splitSentence(string) {
   return string.split(' ');
 }
@@ -27,8 +27,8 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-// Foi pesquisado o seguinte site para entender a função Math.max:
-// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+/* Foi pesquisado o seguinte site para entender a função Math.max (retorna o maior número de um ou mais números):
+  Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max*/
 function highestCount(arrayNumbers) {
   let higherNumber = Math.max.apply(null, arrayNumbers);
   let count = 0;
@@ -42,15 +42,16 @@ function highestCount(arrayNumbers) {
 }
 
 // Desafio 7
-// Não estava passando no último teste, então pesquisei sobre o método Math.abs no seguinte site:
-// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+/* Não estava passando no último teste (porque a comparação tem que ser feita entre valores positivos),
+então pesquisei sobre o método Math.abs (que retorna o valor absoluto) no seguinte site:
+/ Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs*/
 function catAndMouse(mouse, cat1, cat2) {
   let cat1Distance = Math.abs(cat1 - mouse);
   let cat2Distance = Math.abs(cat2 - mouse);
   if (cat1Distance > cat2Distance) {
     return 'cat2';
-  } else if (cat2Distance > cat1Distance) {
-    return 'cat1';
+  } if (cat2Distance > cat1Distance) {
+  return 'cat1';
   } return 'os gatos trombam e o rato foge'; 
 }
 
@@ -60,11 +61,11 @@ function fizzBuzz(arrayNumbers) {
   for (let index = 0; index < arrayNumbers.length; index += 1) {
     if (arrayNumbers[index] % 3 === 0 && arrayNumbers[index] % 5 === 0) {        
       resultado.push('fizzBuzz');
-    }else if (arrayNumbers[index] % 5 === 0) {
+    } else if (arrayNumbers[index] % 5 === 0) {
       resultado.push('buzz');
-    }else if (arrayNumbers[index] % 3 === 0) {
+    } else if (arrayNumbers[index] % 3 === 0) {
       resultado.push('fizz');
-    }else {
+    } else {
       resultado.push('bug!');
     }
   } 
@@ -72,11 +73,52 @@ function fizzBuzz(arrayNumbers) {
 } 
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(encodeString) {
+  let newString = "";
+for (let vogal of encodeString) {
+  if (vogal === "a") {
+    vogal = "1";
+    newString += vogal;
+  } else if (vogal === "e") {
+    vogal = "2";
+    newString += vogal;
+  } else if (vogal === "i") {
+    vogal = "3";
+    newString += vogal;
+  } else if (vogal === "o") {
+    vogal = "4";
+    newString += vogal;
+  } else if (vogal === "u") {
+    vogal = "5";
+    newString += vogal;
+  } else {
+    newString += vogal;
+  }
+} return newString; 
 }
-function decode() {
-  // seu código aqui
+
+function decode(decodeString) {
+  let newWord = "";
+for (let number of decodeString) {
+  if (number === "1") {
+    number = "a";
+    newWord += number;
+  } else if (number === "2") {
+    number = "e";
+    newWord += number;
+  } else if (number === "3") {
+    number = "i";
+    newWord += number;
+  } else if (number === "4") {
+    number = "o";
+    newWord += number;
+  } else if (number === "5") {
+    number = "u";
+    newWord += number;
+  } else {
+    newWord += number;
+  }
+} return newWord; 
 }
 
 module.exports = {
