30ec20037b12c5d7ce80424a90b164453fce2b63
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-06-30 18:05:51 -0300

    #8 fizzBuzz resolvido

--- src/teste.js
+++ src/teste.js
@@ -1,27 +1,24 @@
-// Desafio 7
-// return 'cat2' cat2 esteja a 2 unidades de distância do rato e cat1 esteja a 3 unidades de distância do rato
+/* Desafio 8
+divisível apenas por 3, apresente uma string "fizz";
+divisível apenas por 5, apresente uma string "buzz";
+divisível por 3 e 5, retorne a string "fizzBuzz";
+não dividido por 3 nem por 5, retorne a string "bug!"
+*/
+let arrayy = [2, 15, 7, 9, 45];
 
-// return 'cat1' cat1 esteja a 6 unidades de distância do rato e cat2 esteja a 12 unidades de distância do rato
-
-// return 'os gatos trombam e o rato foge' se gatos estejam na mesma distância do rato
-
-// expect(catAndMouse(1, 0, 2)).toBe('os gatos trombam e o rato foge');
-let mouse = 3;
-let cat1 = 8;
-let cat2 = 1;
-
-function catAndMouse(mouse, cat1, cat2) {
-  let pos1 = Math.abs(cat1 - mouse);
-  let pos2 = Math.abs(cat2 - mouse);
-  if (pos1 === pos2) {
-    return 'os gatos trombam e o rato foge';
+function fizzBuzz(array) {
+  for (let index in array) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      array[index] = 'fizzBuzz';
+    }
+    else if (array[index] % 3 === 0) {
+      array[index] = 'fizz';
+    }
+    else if (array[index] % 5 === 0) {
+      array[index] = 'buzz';
+    } else { array[index] = 'bug!'; }
   }
-  else if (pos1 < pos2) {
-    return 'cat1';
-  } else {
-    return 'cat2';
-  }
-
+  return array;
 }
-  
-console.log(catAndMouse(mouse, cat1, cat2));
+
+console.log(fizzBuzz(arrayy));
