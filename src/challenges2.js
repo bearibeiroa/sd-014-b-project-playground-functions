6c0a7da8136e6dc0d97b5a74c15f32fc92c47456
Bruno Trindade <brunotrndd@gmail.com>
2021-06-29 21:41:15 -0300

    Ajustes lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,7 +4,7 @@ function createObjects(tech, name) {
   this.name = name;
 }
 function techList(technologies, personalName) {
-  let technologieList = [];  
+  let technologieList = [];
   let myList = [];
   for (technologie of technologies.sort()) {
     technologieList.push(technologie);
@@ -13,13 +13,12 @@ function techList(technologies, personalName) {
     myList[index] = new createObjects(technologieList[index], personalName);
   }
   if (technologieList.length === 0) {
-    return 'Vazio!'; 
-  } else {
-    return myList;
-  }   
+    return 'Vazio!';
+  }
+  return myList;
 }
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"));
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber(numbers) {
@@ -28,19 +27,19 @@ function generatePhoneNumber(numbers) {
 
   for (let index = 0; index < 10; index += 1) {
     if (newNumbers[index] > 1) {
-      return 'não é possível gerar um número de telefone com esses valores'
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
   if (numbers.length != 11) {
     return 'Array com tamanho incorreto.';
-  } else {
-    for (let number of numbers) {
-      if (number < 0 || number > 9) {
-        return 'não é possível gerar um número de telefone com esses valores'
-      }
+  }
+  for (let number of numbers) {
+    if (number < 0 || number > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
-  return '(' + numbers[0] + numbers[1] + ') ' + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + '-' + numbers[7] + numbers[8] + numbers[9] + numbers[10];
+
+  return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
 }
 
 console.log(generatePhoneNumber([8, 2, 8, 0, 5, 3, 7, 3, 9, 6, 5]));
@@ -48,7 +47,7 @@ console.log(generatePhoneNumber([8, 2, 8, 0, 5, 3, 7, 3, 9, 6, 5]));
 function findDuplicates(arr) {
   let sorted_arr = arr.slice().sort();
   let results = [];
-  let newNumbers = { '0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0 };
+  let newNumbers = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 };
   for (let i = 0; i < sorted_arr.length - 1; i++) {
     if (sorted_arr[i + 1] === sorted_arr[i]) {
       results.push(sorted_arr[i]);
@@ -56,10 +55,10 @@ function findDuplicates(arr) {
   }
   let index = 0;
   while (index < 10) {
-  for (let result of results) {    
+    for (let result of results) {
       if (result === results[index]) {
         newNumbers[result] += 1;
-      }      
+      }
     }
     index += 1;
   }
@@ -68,7 +67,7 @@ function findDuplicates(arr) {
 }
 
 // Desafio 12
-function triangleCheck(lineA, lineB, lineC) {  
+function triangleCheck(lineA, lineB, lineC) {
   let checkA = [lineB + lineC, Math.abs(lineB - lineC)];
   let checkB = [lineA + lineC, Math.abs(lineA - lineC)];
   let checkC = [lineB + lineA, Math.abs(lineB - lineA)];
@@ -99,23 +98,22 @@ console.log(triangleCheck(2, 14, 8));
 
 // Desafio 13
 function hydrate(string) {
-let matches = string.match(/(\d+)/g);
-let matchesNum = [];
+  let matches = string.match(/(\d+)/g);
+  let matchesNum = [];
   for (let index = 0; index < matches.length; index += 1) {
-    matchesNum[index] = +matches[index]
+    matchesNum[index] = +matches[index];
   }
-let sum = 0;
+  let sum = 0;
   for (let index = 0; index < matches.length; index += 1) {
     sum += matchesNum[index];
   }
   if (sum === 1) {
-    return sum + ' copo de água';
-  } else {  
-return sum + ' copos de água'; 
+    return `${sum} copo de água`;
   }
+  return `${sum} copos de água`;
 }
 
-console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'))
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
