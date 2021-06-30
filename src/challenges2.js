336d97faafe83fb65083b7fcceb57417043cc71a
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-06-30 15:43:53 +0100

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,26 +1,41 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techList, name) {
+    let arrayList = [];
+    techList.sort();
+    if (techList.length === 0) {
+        return "Vazio!";
+    }
+    for (index = 0; index < techList.length; index += 1) {
+        let objectList = {
+            tech: '',
+            name: name,
+        };
+        objectList.tech = techList[index];
+        arrayList.push(objectList);
+    }
+
+    return arrayList;
 }
+
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+    // seu código aqui
 }
 
 module.exports = {
-  generatePhoneNumber,
-  techList,
-  hydrate,
-  triangleCheck,
+    generatePhoneNumber,
+    techList,
+    hydrate,
+    triangleCheck,
 };
