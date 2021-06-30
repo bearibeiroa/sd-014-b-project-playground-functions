c17426f96381e2dec1ec8cab8412a272f2d34c88
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-06-29 23:52:30 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -33,8 +33,26 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    let count = 0;
+    for (let index2 = 0; index2 < array.length; index2 += 1) {
+      if (array[index] === array[index2]) {
+        count += 1;
+      }
+    }
+    if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let phoneNumber = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+  return phoneNumber;
 }
 
 // Desafio 12
