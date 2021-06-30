23ba86b7b3c1549f78c54711e8a70f5b280527c3
Grasiela Gomes <produtoradama@gmail.com>
2021-06-30 11:41:22 -0300

    Refactoring hydrate

--- src/challenges2.js
+++ src/challenges2.js
@@ -62,8 +62,8 @@ function triangleCheck(lineA, lineB, lineC) {
 // Desafio 13
 function hydrate(text) {
   let regex = /\d+/g;
-  let sum = text.match(regex);
-  const total = sum.reduce((currentTotal, item) => parseInt(item, 10) + currentTotal, 0);
+  let numbers = text.match(regex);
+  const total = numbers.reduce((currentTotal, item) => parseInt(item, 10) + currentTotal, 0);
   return (total > 1) ? `${total} copos de água` : `${total} copo de água`;
 }
 
