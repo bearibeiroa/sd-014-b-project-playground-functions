2bf1751d2f563b190e167f62065884eb447ef376
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-29 23:37:11 -0300

    Exercise 11 Complete

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,7 +3,7 @@ function techList(techs, name) {
   if (techs.length > 0) {
     techs = techs.sort();
     let c = [];
-    for (i = 0; i < techs.length; i += 1) {
+    for (let i = 0; i < techs.length; i += 1) {
         let a = {
             tech: techs[i],
             name: name
@@ -17,8 +17,35 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(t) {
+  let erro = false;
+  let number = "";
+  for (let i = 0; i < t.length; i += 1) {
+      if (t.length !== 11) {
+          return ("Array com tamanho incorreto.");
+          erro = true;
+          break;
+      } else if (t[i] < 0 || t[i] > 9) {
+           return ("não é possível gerar um número de telefone com esses valores");
+           erro = true;
+           break;
+      }
+      let r = 0;
+      for (let i2 = 0; i2 < t.length; i2 += 1) {
+          if (t[i2] === t[i]) {
+              r += 1;
+          }
+      }
+      if (r > 2) {
+          return ("não é possível gerar um número de telefone com esses valores");
+          erro = true;
+          break;
+      }
+      r = 0;
+  }
+  if (erro === false) {
+      return (number = number + "(" + t[0] + t[1] + ")" + ' ' + t[2] + t[3] + t[4] + t[5] + t[6] + '-' + t[7] + t[8] + t[9] + t[10]);
+  }
 }
 
 // Desafio 12
