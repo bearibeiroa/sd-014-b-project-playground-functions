2f2bd79933f2df660b7e08656a94d737d3a2ded4
LuanaMB <luanabragaaa@hotmail.com>
2021-06-30 19:18:22 -0300

    commit final

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,33 +16,33 @@ function techList(techs, names) {
   }
   return techList;
 }
-function repeteMais3(numeros){    
-  let resultado =true;  
-  for(let index =0; index<numeros.length; index +=1){  
-    let contador=0;      
-    for(let index2=0; index2<numeros.length;index2+=1){
-      if(numeros[index2]===numeros[index]){
-        contador+=1;
-      }    
-    } 
+function repeteMais3(numeros) {
+  let resultado = true;
+  for (let index = 0; index < numeros.length; index += 1) {
+    let contador = 0;
+    for (let index2 = 0; index2 < numeros.length; index2 += 1) {
+      if (numeros[index2] === numeros[index]) {
+        contador += 1;
+      }
+    }
     //parte feita com ajuda do @Esdras Oliveira-turma 14-B pelo slack
-    if(contador>2){      
-      return resultado;      
-    }     
-    contador=0;        
+    if (contador > 2) {
+      return resultado;
+    }
+    contador = 0;
   }
-  return false;   
+  return false;
 }
-console.log(repeteMais3([1,2,3,5,8,8,8,1]))
+console.log(repeteMais3([1, 2, 3, 5, 8, 8, 8, 1]));
 // Desafio 11
 function generatePhoneNumber(number) {
-  let numberTel = '';    
-  if (number.length != 11)  {
+  let numberTel = '';
+  if (number.length != 11) {
     return 'Array com tamanho incorreto.';
   } else {
-    let erro= repeteMais3(number);  
+    let erro = repeteMais3(number);
     for (let index of number) {
-      if (index < 0 || index > 9 || erro=== true) {
+      if (index < 0 || index > 9 || erro === true) {
         return 'não é possível gerar um número de telefone com esses valores';
       } else {
         numberTel += index;
@@ -55,40 +55,41 @@ function generatePhoneNumber(number) {
     return numeroFormatado;
   }
 }
-console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1]))
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck(lineA,lineB,lineC) {
-  let soma=0;
-  if(lineA<lineB+lineC && lineA>Math.abs(lineB-lineC)){
+function triangleCheck(lineA, lineB, lineC) {
+  let soma = 0;
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
     return true;
-  }
-  else if(lineB<lineA+lineC && lineB>Math.abs(lineA-lineC)){
+  } else if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) {
     return true;
-  }
-  else if(lineC<lineA+lineC && lineC>Math.abs(lineA-lineB)){
+  } else if (lineC < lineA + lineC && lineC > Math.abs(lineA - lineB)) {
     return true;
-  }
-  else{
+  } else {
     return false;
   }
 }
 
 // Desafio 13
 function hydrate(drink) {
-  //pesquisa feita no site: https://www.devmedia.com.br/javascript-replace-substituindo-valores-em-uma-string/39176
-  let glassOfWater = drink.replace(/[^0-9]/g,[]);   
-  console.log(glassOfWater)
-  let inteiro= []; 
-  for(i=0;i<glassOfWater.length;i+=1) {
-    let numero = parseInt(glassOfWater[i],10);
-    inteiro[i]+= numero;
-  }  
-  console.log(inteiro)
-  let soma= 0; 
-  return soma + 'copos de água';
+  //pesquisa sobre replace feita no site: https://www.devmedia.com.br/javascript-replace-substituindo-valores-em-uma-string/39176
+  let glassOfWater = drink.replace(/[^0-9]/g, []);
+  let inteiro = [];
+  for (i = 0; i < glassOfWater.length; i += 1) {
+    let numero = parseInt(glassOfWater[i], 10);
+    inteiro[i] = numero;
+  }
+  let soma = 0;
+  for (let index = 0; index < inteiro.length; index += 1) {
+    soma = soma + inteiro[index];
+  }
+  if (soma > 1) {
+    return soma + ' copos de água';
+  }
+  return soma + ' copo de água';
 }
-console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'))
+console.log(hydrate('0 cachaça, 0 cervejas e 0 copo de vinho'));
 module.exports = {
   generatePhoneNumber,
   techList,
