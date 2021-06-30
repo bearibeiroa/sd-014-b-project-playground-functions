4b8161b4a9d5ac212460d39ebaddc69abd291729
Mateus Gambaro <magambaro@hotmail.com>
2021-06-30 14:26:01 -0300

    Requisito #10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
 function techList() {
   // seu código aqui
+  let novaArray = array.sort();
+  let objectArray = [];
+  
+  if (array.length === 0){
+    return "Vazio!";
+  }
+  else {
+    for (let index = 0; index < novaArray.length; index += 1){
+      let objeto = {
+        tech: novaArray[index],
+        name: name
+      };
+      objectArray.push(objeto);
+    }
+  }
+  return objectArray;
 }
 
 // Desafio 11
