9eb1dcfbdd9332ea5e67cf011cdeef13c917b33c
LuanaMB <luanabragaaa@hotmail.com>
2021-06-30 09:55:41 -0300

    exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,11 +15,17 @@ function techList(techs, names) {
   }
   return techList;
 }
-console.log(techList(['html','css','react'],"luana"))
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {  
+  let numberTel='';      
+  for(let index of number){   
+    numberTel+= index
+  }  
+  const numeroFormatado=numberTel.replace(/(\d{2})?(\d{5})?(\d{4})/,"($1)$2-$3")
+  return numeroFormatado;
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck() {
@@ -37,3 +43,8 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+
+a='12345678901';
+let resultado= a.replace(/(\d{2})?(\d{5})?(\d{4})/,"($1)$2-$3")
+console.log(resultado)
