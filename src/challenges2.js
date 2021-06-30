b2b151dfad5d1fbe3e883b1f3c481f4fe05a6021
jarana-2020 <julioaranabr@yahoo.com.br>
2021-06-30 11:44:18 -0300

    Requisito #11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,9 +19,35 @@ function techList(tech, name) {
 techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas");
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phone) {
+
+  if (phone.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let index in phone) {
+    if (phone[index] < 0 || phone[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  for (let key in phone) {
+    let count = 0;
+    for (let secondKey in phone) {
+      if (phone[key] === phone[secondKey]) {
+        count += 1;
+        if (count > 2) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+  }
+  let mask = '(xx) xxxxx-xxxx';
+  phone.forEach(element => {
+    mask = mask.replace('x', element)
+  });
+  return mask;
 }
+generatePhoneNumber([9, 2, 3, 0, 5, -6, 7, 8, -7, 0, 1, 10]);
 
 // Desafio 12
 function triangleCheck() {
