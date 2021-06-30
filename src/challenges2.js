021d70021fd90d0ac52c3417a75160897620e249
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-06-30 20:31:59 -0300

    Requisito #10?

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,6 @@
 // Desafio 10
 function techList(techs, name) {
-  let techList = [];
+  let listedTechs = [];
   let ordTechs = techs.sort();
   if (techs.length < 1) return 'Vazio!';
   for (let index in ordTechs) {
@@ -9,12 +9,27 @@ function techList(techs, name) {
       name: name,
     });
   }
-  return techList;
+  return listedTechs;
   // seu código aqui
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(number) {
+  if (number.length !== 11) return 'Array com tamanho incorreto.';
+  for (let index = 0; index < number.length; index += 1) {
+    if (number[index] < 0 || number[index] > 9) return 'não é possível gerar um número de telefone com esses valores';
+    let numberX = 1;
+    for (let indexN = 0; indexN < number.length; indexN += 1) {
+      if (number[index] === number[indexN]) {
+        numberX += 1;
+      }
+      if (numberX > 3) return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  var phoneNumber = ['(nn) nnnnn-nnnn']
+  for (let index = 0; index < phoneNumber.length; index += 1) {
+    phoneNumber = phoneNumber.replace('n', number[index]);
+  } return phoneNumber;
   // seu código aqui
 }
 
