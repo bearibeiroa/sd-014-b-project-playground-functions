852925e80f2a02f65cf5545c12992ba4f66e2708
Denis Espanhol <denisgfespanhol@gmail.com>
2021-06-30 03:06:10 -0300

    Oitavo requisito do Playground Functions.

--- src/challenges.js
+++ src/challenges.js
@@ -67,8 +67,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let array2 = [];
+  for (index = 0; index < array.length; index += 1) {
+    if ((array[index] % 3) === 0 && (array[index] % 5) === 0) {
+      array2 +='fizzBuzz,';
+    } else if ((array[index] % 5) === 0) {
+      array2 += 'buzz,';
+    } else if (((array[index] % 3) === 0)) {
+      array2 += 'fizz,';
+    } else {
+      array2 += 'bug!,';
+    }
+  }
+  return array2.split(',', array.length);
 }
 
 // Desafio 9
