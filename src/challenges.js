cfb4e7ba06160a03f2d4d872407df1de1295297f
Rafael Veiga <rafaelveig@hotmail.com>
2021-06-29 21:56:10 -0300

    requisito 8 incompleto

--- src/challenges.js
+++ src/challenges.js
@@ -3,22 +3,20 @@ function compareTrue(valor1, valor2) {
   if (valor1 === true && valor2 === true) {
     return true;
   } else {
-    return false
+    return false;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) / 2
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence(frase){
-  let palavra = frase.split(" ") 
+function splitSentence(frase) {
+  let palavra = frase.split(" ")
   return palavra
 }
-
-
 
 // Desafio 4
 function concatName(itens) {
@@ -35,14 +33,14 @@ function footballPoints(wins, ties) {
 function highestCount(numeros) {
   let maiorNum = numeros[0];
   let quantidade = 0
-  for (let cont = 0; cont < numeros.length; cont ++){
-    if (numeros[cont]>maiorNum){
+  for (let cont = 0; cont < numeros.length; cont++) {
+    if (numeros[cont] > maiorNum) {
       maiorNum = numeros[cont]
     }
   }
-  for (let cont =0; cont < numeros.length; cont ++){
-    if(maiorNum === numeros[cont]){
-      quantidade ++
+  for (let cont = 0; cont < numeros.length; cont++) {
+    if (maiorNum === numeros[cont]) {
+      quantidade++
     }
   }
   return quantidade;
@@ -54,8 +52,20 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numeros) {
+  let resultado = [];
+  for (let i = 0; i <= numeros.length; i++) {
+    if (numeros[i] % 3 == 0) {
+      resultado.push("fizz")
+    } else if (numeros[i] % 5 == 0) {
+      resultado.push("buzz")
+    } else if ((numeros[i] % 3 == 0) && (numeros[i] % 5 == 0)) {
+      resultado.push("fizzBuzz")
+    } else {
+      resultado.push("bug!")
+    }
+  }
+  return resultado
 }
 
 // Desafio 9
