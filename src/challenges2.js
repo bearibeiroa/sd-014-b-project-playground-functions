e75a75cdc2ba59cdefe0adfe828702d01a9a3f03
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-06-30 08:41:36 -0300

    Desafio #10 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,23 @@
 // Desafio 10
+
 function techList(techArray, name) {
-  if (techArray === []) {
-    let vazio = 'Vazio!';
-    return vazio;
-  } else {
-    let listaTechName = [];
-    for (let key in techArray) {
-      let objetoTechName = {
-        tech: techArray[key],
-        name: name,
-      };
-      listaTechName.push(objetoTechName);
-    }
-    return listaTechName.sort((a, b) => (a.tech > b.tech) ? 1 : -1);
-    // Referência para a linha acima: https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
+  let listaTechName = [];
+  for (let index = 0; index < techArray.length; index += 1) {
+    let objetoTechName = { tech: techArray[index], name: name };
+    listaTechName.push(objetoTechName);
   }
+  if (listaTechName.length === 0) {
+    return 'Vazio!';
+  }
+  return listaTechName.sort((a, b) => (a.tech > b.tech) ? 1 : -1);
+    // Referência para a linha acima: https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
 }
+
+let testArray = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+let testArray2 = [];
+let nomeArray = "Lucas";
+console.log(techList(testArray2, nomeArray));
+console.log(techList(testArray, nomeArray));
 
 // Desafio 11
 function generatePhoneNumber() {
