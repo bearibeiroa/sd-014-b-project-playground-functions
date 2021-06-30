fce4665810c88e2c679efc52bde17a483f2fb291
Vinicius <vinicius@pop-os.localdomain>
2021-06-30 10:58:03 -0300

    Correções

--- src/challenges.js
+++ src/challenges.js
@@ -85,18 +85,18 @@ function fizzBuzz(arrayFzBz) {
 function encode(mensagem) {
   let mensagemNova = [];
   for (let index = 0; index < mensagem.length; index += 1) {
-    if ( mensagem[index] === 'a') {
-    mensagemNova[index] = 1;
-  } else if ( mensagem[index] === 'e') {
-    mensagemNova[index] = 2;
-  } else if ( mensagem[index] === 'i') {
-    mensagemNova[index] = 3;
-  } else if ( mensagem[index] === 'o') {
-    mensagemNova[index] = 4;
-  } else if ( mensagem[index] === 'u') {
-    mensagemNova[index] = 5;
+    if (mensagem[index] === 'a') {
+      mensagemNova[index] = 1;
+  } else if (mensagem[index] === 'e') {
+      mensagemNova[index] = 2;
+  } else if (mensagem[index] === 'i') {
+      mensagemNova[index] = 3;
+  } else if (mensagem[index] === 'o') {
+      mensagemNova[index] = 4;
+  } else if (mensagem[index] === 'u') {
+      mensagemNova[index] = 5;
   }  else {
-    mensagemNova[index] = mensagem[index]; 
+      mensagemNova[index] = mensagem[index]; 
   }
   }  
   return mensagemNova.join('');
@@ -105,18 +105,18 @@ function encode(mensagem) {
 function decode(mensagemDecode) {
   let mensagemNovaDecode = [];
   for (let index = 0; index < mensagemDecode.length; index += 1) {
-    if ( mensagemDecode[index] === '1') {
-    mensagemNovaDecode[index] = 'a';
-  } else if ( mensagemDecode[index] === '2') {
-    mensagemNovaDecode[index] = 'e';
-  } else if ( mensagemDecode[index] === '3') {
-    mensagemNovaDecode[index] = 'i';
-  } else if ( mensagemDecode[index] === '4') {
-    mensagemNovaDecode[index] = 'o';
-  } else if ( mensagemDecode[index] === '5') {
-    mensagemNovaDecode[index] = 'u';
+    if (mensagemDecode[index] === '1') {
+      mensagemNovaDecode[index] = 'a';
+  } else if (mensagemDecode[index] === '2') {
+      mensagemNovaDecode[index] = 'e';
+  } else if (mensagemDecode[index] === '3') {
+      mensagemNovaDecode[index] = 'i';
+  } else if (mensagemDecode[index] === '4') {
+      mensagemNovaDecode[index] = 'o';
+  } else if (mensagemDecode[index] === '5') {
+      mensagemNovaDecode[index] = 'u';
   }  else {
-    mensagemNovaDecode[index] = mensagemDecode[index]; 
+      mensagemNovaDecode[index] = mensagemDecode[index]; 
   }
   }  
   return mensagemNovaDecode.join('');
