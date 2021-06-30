bb8bb2ea0662c1b61f56d9b769b8349891fafd02
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-06-30 00:06:03 -0300

    Requisito #12

--- src/challenges2.js
+++ src/challenges2.js
@@ -73,8 +73,13 @@ function generatePhoneNumber(numbers) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA > lineB + lineC || lineB > lineA + lineC || lineC > lineA + lineB) {
+    return false;
+  } else if (lineA < Math.abs(lineB - lineC) || lineB < Math.abs(lineA - lineC) || lineC < Math.abs(lineA - lineB)) {
+    return false;
+  }
+  return true;
 }
 
 // Desafio 13
