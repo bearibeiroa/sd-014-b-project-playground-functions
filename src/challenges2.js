16ed62f83481ad8f722150aae191b74e4daef737
Paulo França <paulovinicius.franca@gmail.com>
2021-06-29 21:12:07 -0300

    Resolução do Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,28 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let counter = 0;
+  if (array.length > 11 || array.length < 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    for (let index = 0; index < array.length; index += 1) {
+      if (array[index] > 9 || array[index] < 0) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      counter = 0;
+      for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
+        if (array[index] === array[secondIndex]) {
+          counter += 1;
+          if (counter >= 3) {
+            return 'não é possível gerar um número de telefone com esses valores';
+          }
+        }
+      }
+    }
+    let number = '(' + array[0] + array [1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+    return number;
+  }
 }
 
 // Desafio 12
