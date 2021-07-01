ae20fdb903a89ed0081b89ada56921ddf2be6103
Denis Espanhol <denisgfespanhol@gmail.com>
2021-07-01 00:30:37 -0300

    Décimo requisito do Playground Functions.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,27 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+let test = [];
+function techList(array, name) {
+  let objectList = [];
+  if (array.length > 0) {
+    for (let index = 0; index < array.length; index += 1) {
+      objectList.push({ tech: array[index], name: name,});
+    }
+    return objectList.sort(function(a, b) {
+      let nameA = a.tech.toUpperCase();
+      let nameB = b.tech.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  return 'Vazio!';
 }
+
+console.log(techList(test, 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
