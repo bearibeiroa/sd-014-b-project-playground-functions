0c30d0121ba10e9d67d6e06141b26ea4a02207f9
andremorsch <andremorsch37@gmail.com>
2021-07-01 17:08:59 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,8 +4,46 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(cel) {
+  if (cel.length !== 11) {
+    return "Array com tamanho incorreto."
+  } else {for (let i = 0; i < cel.length; i += 1) {
+    if (cel[i] < 0 || cel [i] > 9) {
+      return "não é possível gerar um número de telefone com esses valores"
+      }
+    }
+  } 
+  for (let i = 0; i < cel.length; i += 1) {
+    let qntNum = 0
+    for (let index = 0; index < cel.length; index += 1) {
+      if (cel[i] === cel[index]) {
+        qntNum +=1
+      }
+      if (qntNum >= 3) {
+        return "não é possível gerar um número de telefone com esses valores"
+      }
+    }
+  }
+  let celular = ""
+  for (let i = 0; i < cel.length; i += 1) {
+    switch (i) {
+      case 0:
+        celular = "(" + cel[i]
+        break
+
+      case 1:
+        celular = celular + cel[i] + ") "
+        break
+      
+      case 6:
+        celular = celular + cel[i] + "-"
+        break
+
+      default:
+        celular += cel[i]
+    }
+  }
+  return celular
 }
 
 // Desafio 12
