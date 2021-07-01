c5888e0d649dd778499359a08932dab6e066e173
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-06-30 22:55:39 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,9 +22,39 @@ function techList(tecnologia,name) {
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  
+function generatePhoneNumber(array) {
+  let phoneNumber = '(';
+  let qtd = 0;
+  if (array.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let cont = 0; cont < array.length; cont += 1) {
+    qtd = 0;
+    for (let cont2 = 0; cont2 < array.length; cont2 += 1) {
+      if (array[cont] === array[cont2]) {
+        qtd += 1;
+      }
+      if (qtd > 2) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+
+      if (array[cont] < 0 || array[cont] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      
+    }
+      if (cont === 2) {
+        phoneNumber += ') ' + array[cont];
+      } else if (cont === 7) {
+        phoneNumber += '-' + array[cont];
+      } else {
+        phoneNumber += array[cont];
+      }
+  }
+  return phoneNumber;
 }
+
+
 
 // Desafio 12
 function triangleCheck() {
--- src/teste.js
+++ src/teste.js
@@ -1,23 +1,34 @@
-function catAndMouse(mouse,cat1,cat2) {
-  
-  let distancia1 = cat1 - mouse;
-  let distancia2 = cat2 - mouse;
-  
-  if (distancia1 < 0){
-    distancia1 = distancia1 *-1;
+function generatePhoneNumber(array) {
+  let phoneNumber = '(';
+  let qtd = 0;
+  if (array.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let cont = 0; cont < array.length; cont += 1) {
+    qtd = 0;
+    for (let cont2 = 0; cont2 < array.length; cont2 += 1) {
+      if (array[cont] === array[cont2]) {
+        qtd += 1;
+      }
+      if (qtd > 2) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
 
-  }if  (distancia2 < 0){
-    distancia2 = distancia2 *-1;
+      if (array[cont] < 0 || array[cont] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      
+    }
+      if (cont === 2) {
+        phoneNumber += ')' + array[cont];
+      } else if (cont === 7) {
+        phoneNumber += '-' + array[cont];
+      } else {
+        phoneNumber += array[cont];
+      }
   }
-  
-  if(distancia1 === distancia2){
-    
-    return 'os gatos trombam e o rato foge';
-  }else if (distancia1 > distancia2){
-    return 'cat2';
-  }else{
-     return 'cat1';
-  }
-  ;
+  return phoneNumber;
 }
-console.log(catAndMouse(5,7,6))
+let teste = [2, 1, 9, 5, 7, 3, 7, 8, 8, 6, 6];
+
+console.log(generatePhoneNumber(teste));
