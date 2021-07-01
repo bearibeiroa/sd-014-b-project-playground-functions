98c32d605b137b996b4d710331dac23b213e178c
Thyara <thyaranonato@outlook.com>
2021-07-01 14:35:55 -0300

    Adiciona o Desafio #10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,19 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techName, name) {
+  let techListName = [];
+  let ordTechList = techName.sort();
+  if (techName.length === 0) {
+    return ('Vazio!');
+  } 
+  for (let index = 0; index < ordTechList.length; index += 1) {
+    let object = {
+      tech: ' ',
+      name: name,
+    }
+    object.tech = ordTechList[index];
+    techListName.push(object);
+  }
+  return techListName;
 }
 
 // Desafio 11
