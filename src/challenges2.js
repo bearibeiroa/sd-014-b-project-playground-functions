d25d66d49f85765246db6c5ae80e6fa1bf40e2e3
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2021-06-30 22:48:23 -0300

    project playground functions exercicio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -28,10 +28,9 @@ function generatePhoneNumber(phone) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
   }
-  
 }
 // Desafio 12
-// comando Math.abs explicado no arquivo challenges, desafio 7. 
+// comando Math.abs explicado no arquivo challenges, desafio 7.
 function triangleCheck(lineA, lineB, lineC) {
   let ab = Math.abs(lineA + lineB);
   let ac = Math.abs(lineA + lineC);
@@ -43,8 +42,19 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
+// função parseInt(string, base) serve para converter uma string em número, referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/parseInt
 function hydrate(quantidadesDeBebida) {
-  // seu código aqui
+  let contador = 0;
+  for (let index = 0; index < quantidadesDeBebida.length; index += 1) {
+    if (quantidadesDeBebida[index] >= 1 && quantidadesDeBebida[index] <= 9) {
+      contador += parseInt(quantidadesDeBebida[index], 10);
+    }
+  }
+  if (contador === 1) {
+    return '1 copo de água';
+  }
+  let retorno = `${contador} copos de água`;
+  return retorno;
 }
 
 module.exports = {
