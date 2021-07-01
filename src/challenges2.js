741717e804cf52e16d96a0b184b479df3808edd7
Paulo França <paulovinicius.franca@gmail.com>
2021-07-01 17:15:22 -0300

    Resolve o exercício 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,16 +4,14 @@ function techList(array, name) {
   let objectArray = [];
   if (array.length === 0) {
     return 'Vazio!';
-  } else {
-    for (let index = 0; index < newArray.length; index += 1) {
-      let object = {
-        tech: newArray[index],
-        name: name,
-      };
-      objectArray.push(object);
-    }
+  } for (let index = 0; index < newArray.length; index += 1) {
+    let object = {
+      tech: newArray[index],
+      name: name,
+    };
+    objectArray.push(object);
   }
-  return objectArray;
+  return objectArray;  
 }
 
 // Desafio 11
@@ -36,7 +34,7 @@ function generatePhoneNumber(array) {
         }
       }
     }
-    let number = '(' + array[0] + array [1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+    let number = '(' + array[0] + array [1] + ') ' + + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
     return number;
   }
 }
@@ -51,14 +49,22 @@ function triangleCheck(lineA, lineB, lineC) {
   let difLineBC = Math.abs(lineB - lineC);
   if (lineC < sumLineAB && lineB < sumLineAC && lineA < sumLineBC && lineC > difLineAB && lineB > difLineAC && lineA > difLineBC) {
     return true;
-  } else {
-    return false;
-  }
+  } return false;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let regExp = /\d/g; // Feito com auxílio do tutorial encontrado em https://www.youtube.com/watch?v=pfkkdzeyx6U
+  let array = string.match(regExp); /* O tutorial citado também ajuda nessa etapa, mas encontrei uma explicação mais detalhada em https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match */
+  let number = 0;
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    number = Number(array[index]); /* Retirado de https://www.w3schools.com/js/js_number_methods.asp */
+    sum += number;
+  }
+  if (sum === 1) {
+    return sum + ' copo de água';
+  } return sum + ' copos de água';
 }
 
 module.exports = {
