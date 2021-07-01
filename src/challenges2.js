2da3d440857c17e6ac15e66c75c5d4e0a74c757e
eli <leonardohcds@gmail.com>
2021-07-01 19:06:45 -0300

    challenge 11 done

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,18 +7,46 @@ function techList(tech, name) {
     return 'Vazio!';
   }
   for (let value of tech) {
-   techsToLearn.push({
-     tech: value,
-     name: name,
-   });
+    techsToLearn.push({
+      tech: value,
+      name: name,
+    });
   }
   return techsToLearn;
 }
 
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let phoneNum = '';
+
+  if (numbers.length != 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    for (let value of numbers) {
+      let currentNum = 0;
+
+      if (value < 0 || value > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      for (let value2 of numbers) {
+        if (value2 == value) {
+          currentNum += 1;
+        }
+        if (currentNum >= 3) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+  }
+
+  numbers.unshift('('); //
+  numbers.splice(3, 0, ') '); // coded by Victor Veloso, 14 B
+  numbers.splice(9, 0, '-'); // with: https://developer.mozilla.org/pt-BR/docs/Web/ JavaScript/Reference/Global_Objects/Array/splice
+
+  for (let value of numbers) {
+    phoneNum += value;
+  }
+  return phoneNum;
 }
 
 // Desafio 12
