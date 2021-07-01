dd38d78bd3d497ee37d6299e2243a9c52540d3d0
Emmanuel <EmmanuelRodriguesBoo@gmail.com>
2021-07-01 13:12:01 -0300

    Projeto 2 completo

--- src/challenges.js
+++ src/challenges.js
@@ -1,49 +1,126 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  if(value1 === true && value2 === true){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let triangle = (base * height)/2;
+  return triangle;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(text) {
+  let txt = text.split(' ');
+  return txt;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(names) {
+  let array = [...names]
+  let result = [];
+  result.push(array[array.length - 1])
+  result.push(array[0])
+  return result.join(', ');
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let game = (wins * 3) + ties;
+  return game;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(val) {
+  let num = [...val];
+  let maior = -10000000000;
+  let quant = 0;
+  for(let index = 0; index < num.length; index += 1){
+    if(num[index] > maior){
+      maior = num[index];
+    }  
+  }
+  for(let i = 0; i < num.length; i += 1){
+    if(maior === num[i]){
+      quant += 1;
+    }
+  }
+  return quant;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1 = mouse - cat1;
+  let distCat2 = mouse - cat2;
+  let distFinal = "";
+    if(distCat1 < distCat2){
+      distFinal = "cat1";
+    }else if(distCat2 < distCat1){
+      distFinal = "cat2";
+    }else{
+      distFinal = "os gatos trombam e o rato foge";
+    }
+  return distFinal;
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arr) {
+  let num = [...arr];
+  let fzb = []
+  for(let index = 0; index < num.length; index += 1){
+    if(num[index]%3 === 0 && num[index]%5 === 0){
+      fzb.push("fizzBuzz");
+    }else if(num[index]%5 === 0){
+      fzb.push("buzz");
+    }else if(num[index]%3 === 0){
+      fzb.push("fizz")
+    }else{
+      fzb.push("bug!")
+    }
+  }
+  return fzb
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str1) {
+  let array = [...str1];
+  for(let index = 0; index < array.length; index += 1){ 
+    if(array[index] === "a"){
+      array[index] = 1;
+    }else if(array[index] === "e"){
+      array[index] = 2;
+    }else if(array[index] === "i"){
+      array[index] = 3;
+    }else if(array[index] === "o"){
+      array[index] = 4;
+    }else if(array[index] === "u"){
+      array[index] = 5;
+    }
+  }
+  return array.join('',',')
 }
-function decode() {
-  // seu código aqui
+
+function decode(str2) {
+  
+  let array2 = [...str2];
+  for(let index = 0; index < array2.length; index += 1){ 
+    if(array2[index] === "1"){
+      array2[index] = "a";
+    }else if(array2[index] === "2"){
+      array2[index] = "e";
+    }else if(array2[index] === "3"){
+      array2[index] = "i";
+    }else if(array2[index] === "4"){
+      array2[index] = "o";
+    }else if(array2[index] === "5"){
+      array2[index] = "u";
+    }
+  }
+  return array2.join('',',')
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,87 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arr ,name) {
+  let arrays = [...arr];
+  arrays.sort();
+  let techs = [];
+  if(arrays.length <= 0){
+    return "Vazio!";
+  }else{
+    for(let i = 0; i < arrays.length; i += 1){
+      techs.push(
+        {
+          tech: arrays[i],
+          name: name
+        })
+    }
+    return techs;
+  }
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phone) {
+  let phoneNumber = [...phone];
+  let num = ["(", phoneNumber[0], phoneNumber[1],")"," ", phoneNumber[2], phoneNumber[3],phoneNumber[4], phoneNumber[5], phoneNumber[6], "-", phoneNumber[7],phoneNumber[8], phoneNumber[9],phoneNumber[10]];
+  if(phoneNumber.length != 11){
+    return "Array com tamanho incorreto.";
+  }else{
+    for(let i = 0; i < phoneNumber.length; i += 1){
+      if(phoneNumber[i] < 0 || phoneNumber[i] > 9 ){
+        return "não é possível gerar um número de telefone com esses valores" ;
+      }
+    }
+    for(let i = 0; i < phoneNumber.length; i += 1){
+      let repNum = 0;
+      for(let index = 0; index < phoneNumber.length; index += 1){
+        if(phoneNumber[i] === phoneNumber[index]){
+          repNum += 1;
+        }
+      }
+      if(repNum > 2){
+        return"não é possível gerar um número de telefone com esses valores"
+      }
+    }
+  }
+  return num.join('',',');
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let diffAB = Math.abs(lineA - lineB);
+  let diffBC = Math.abs(lineB - lineC);
+  let diffAC = Math.abs(lineC - lineA);
+  let sumAB = lineA + lineB;
+  let sumBC = lineB + lineC;
+  let sumAC = lineC + lineA;
+  let resultA = lineA > diffBC && lineA < sumBC;
+  let resultB = lineB > diffAC && lineB < sumAC;
+  let resultC = lineC > diffAB && lineC < sumAB;
+  let result = false;
+
+  if(lineA > sumBC || lineB > sumAC || lineC > sumAB)
+    return false;
+  else if(lineA < diffBC || lineB < diffAC || lineC < diffAB)
+    return false;
+  else if(resultA === true || resultB === true || resultC === true)
+    return true;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(str) {
+  let n = /\d+/g;
+  let num = str.match(n);
+  let water = 0;
+  
+  for(let i = 0; i < num.length; i += 1){
+      water += parseInt(num[i]);
+  }
+
+  if(water === 1)
+    return water + " copo de água";
+  else
+    return water + " copos de água";
+
 }
+hydrate("1 cerveja e 3 afff")
 
 module.exports = {
   generatePhoneNumber,
