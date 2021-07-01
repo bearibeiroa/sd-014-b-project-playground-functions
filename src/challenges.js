4890a847e7f27ee531f75861a3781104a81b8977
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-06-30 22:50:47 -0300

    Requisito #9

--- src/challenges.js
+++ src/challenges.js
@@ -97,12 +97,62 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+  let newString = '';
+  for (let index = 0; index < string.length; index += 1) {
+    let valorAtual = string[index];
+    switch (valorAtual) {
+      case 'a':
+        newString += '1';
+        break;
+      case 'e':
+        newString += '2';
+        break;
+      case 'i':
+        newString += '3';
+        break;
+      case 'o':
+        newString += '4';
+        break;
+      case 'u':
+        newString += '5';
+        break;
+      default:
+        newString += valorAtual;
+        break;
+    }
+  }
+  return newString;
 }
 
-function decode() {
+function decode(string) {
   // seu código aqui
+  let newString = '';
+  for (let index = 0; index < string.length; index += 1) {
+    let valorAtual = string[index];
+    switch (valorAtual) {
+      case '1':
+        newString += 'a';
+        break;
+      case '2':
+        newString += 'e';
+        break;
+      case '3':
+        newString += 'i';
+        break;
+      case '4':
+        newString += 'o';
+        break;
+      case '5':
+        newString += 'u';
+        break;
+      default:
+        newString += valorAtual;
+        break;
+    }
+  }
+  return newString;
 }
 
 module.exports = {
