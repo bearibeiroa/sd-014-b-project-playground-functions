97fbf9ff5c509845bf76d1e5153028e297a2c3a4
icaroemanuel <icaro.emanuel@outlook.com>
2021-07-02 14:58:54 -0300

    challenge#10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,30 @@
+// Função que ordena objetos em ordem alfabética crescente
+function sortObject(object) {
+/*
+*O código a seguir foi extraido de um tutorial disponível no blog https://www.edsonemiliano.com.br/blog/como-ordenar-uma-array-de-objetos-com-javascript-sort/
+*A intenção foi ordenar o objeto em ordem alfabética utilizando a função sort() e operador condicional ternário
+*/
+  object.sort(function (keyOne, keytwo) {
+    return (keyOne.tech > keytwo.tech) ? 1 : ((keytwo.tech > keyOne.tech) ? -1 : 0);
+  });
+}
+
 // Desafio 10
-function techList() {
+function techList(technologies, name) {
   // seu código aqui
+  let listOfTechs = [];
+  for (let index = 0; index < technologies.length; index += 1) {
+    listOfTechs.push(
+      {
+        tech: technologies[index],
+        name,
+      },
+    );
+  };
+  if (listOfTechs.length === 0) return 'Vazio!';
+  // O código a seguir foi extraido de um tutorial disponível no blog https://www.edsonemiliano.com.br/blog/como-ordenar-uma-array-de-objetos-com-javascript-sort/.A intenção foi ordenar o objeto em ordem alfabética utilizando a função sort() e operador condicional ternário.
+  sortObject(listOfTechs);
+  return listOfTechs;
 }
 
 // Desafio 11
