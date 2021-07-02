3d0a0645f2529d4ac0dbbde106d7adc7b373097d
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-07-02 18:12:50 -0300

    #11generatePhoneNumber resolvido

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,42 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  // validando se array pode ser usado
+  // se mais de 11 numeros tamanho incorreto
+  if (array.length != 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  
+  // numero deve ser maior que 0 e menor que 9
+  for (let numero in array) {
+    if (array[numero] < 0 || array[numero] > 9) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    else {
+      // numero não deve repetir 3x ou mais
+      let count = 0;
+      for (compare in array) {
+        if (array[numero] === array[compare]) {
+          count += 1;
+        }
+      }
+      if (count >= 3) {
+        return "não é possível gerar um número de telefone com esses valores"
+      }
+    }
+  }
+  // array é valido vamos formata-lo:
+  // array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
+  array.splice(0, 0, '(');
+  array.splice(3, 0, ')');
+  array.splice(4, 0, ' ');
+  array.splice(10, 0, '-');
+  let phone = ''
+  for (index in array) {
+    phone += array[index]
+  }
+  return phone;
 }
 
 // Desafio 12
