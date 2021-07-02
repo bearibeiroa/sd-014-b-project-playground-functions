05634332479d0c79be8795f4ac0e68d6fbcaef55
Gabriel Takaki <gabriel.f.takaki@gmail.com>
2021-07-02 16:32:48 -0300

    requisitos obrigatorios 100%

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,26 +1,101 @@
 // Desafio 10
-function techList() {
+function techList(techs, nome) {
+    let listaTech = [];
+    techs.sort();
+    if (techs.length <= 0) {
+      return "Vazio!";
+    } else {
+      for(let indice in techs){
+        listaTech.push({
+          tech: techs[indice],
+          name: nome,
+        })
+      }
+      return listaTech;
+    }
+  }
+
+
   // seu código aqui
-}
+
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array11) {
+  let numeroNovo = "";
+  let r = 0;
+  for (let i = 0; i < array11.length; i += 1) {
+            for (let i2 = 0; i2 < array11.length; i2 += 1) {
+                if (array11[i2] === array11[i]) {
+                    r += 1;
+                }
+            }
+            if (r === 3) {
+                return ("não é possível gerar um número de telefone com esses valores");
+            }
+   else if (array11[i] <0){
+     return"não é possível gerar um número de telefone com esses valores"
+   }
+   else if (array11[i] > 9){
+    return "não é possível gerar um número de telefone com esses valores"
+  }
+   else if (array11.length != 11){
+     return "Array com tamanho incorreto."
+   }
+    if ([i] === 0){
+      numeroNovo =+ "(" + array11[i]
+    }
+    else if ([i] === 1){
+      numeroNovo =+ array11[i] + ")"
+    }
+     else if ([i] === 6){
+    numeroNovo =+ array11[i] + "-"
+  }
+  else {
+    numeroNovo =+ array11[i]
+  }
+  }
+  return numeroNovo;
 }
+   
+  
+
+
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let soma = false;
+  let absoluto = false;
+
+  if (lineA > lineB +lineC){
+    let soma = false;
+  }
+  else if (lineB > lineC + lineA){
+    let soma = false;
+  }
+  else if (lineC > lineB + lineA){
+    let soma = false;
+  }
+  else {
+    let soma = true;
+  }
+
+  return soma
 }
+  
+
+
+
+  // seu código aqui
+
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(lineA, lineB, lineC) {
 }
+  // seu código aqui
 
 module.exports = {
   generatePhoneNumber,
   techList,
   hydrate,
   triangleCheck,
-};
+}
