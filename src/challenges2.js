bc2e04c4039625aadfc22816ee3acc106dcf8f0f
dhenycruz <dheniarley.ds@gmail.com>
2021-07-02 02:38:19 -0300

    Requisito #10.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,26 @@
 // Desafio 10
-function techList() {
+function techList(arrayTech, name) {
   // seu código aqui
+  if (arrayTech.length === 0) {
+    return 'Vazio!';
+  }
+  for (let index = 0; index < arrayTech.length; index += 1) {
+    arrayTech[index] = {
+      'tech': arrayTech[index],
+      'name': name
+    };
+  }
+
+  return arrayTech.sort(function (a, b) {
+    if (a.tech > b.tech) {
+      return 1;
+    }
+    if (a.tech < b.tech) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
 }
 
 // Desafio 11
