97ff58207ebd98d5757326988f20ba6122a8a773
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2021-07-02 10:43:56 -0300

    Requisito 6

--- src/challenges.js
+++ src/challenges.js
@@ -8,30 +8,38 @@ function compareTrue(arg1, arg2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) / 2
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(str) {
-  return str.split(' ')
+  return str.split(' ');
 }
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length -1] + ", " + array[0]
+  return array[array.length -1] + ", " + array[0];
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let pontos = 0
-  pontos += wins * 3
-  pontos += ties * 1
-  return pontos
+  let pontos = 0;
+  pontos += wins * 3;
+  pontos += ties * 1;
+  return pontos;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(narray) {
+  narray.sort();
+  let highest = narray[narray.length - 1];
+  let counter = 0;
+  for(i in narray)
+  {
+     if (narray[i] === highest)
+      counter++;
+  }
+  return counter;
 }
 
 // Desafio 7
