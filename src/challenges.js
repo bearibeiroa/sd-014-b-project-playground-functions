9c881b3f1debfb8bfdbb2616dbbd6c092ba515a4
Deyvison <deyvisonn10@hotmail.com>
2021-07-02 11:33:35 -0300

    requisito1

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,17 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1, valor2) {
+ 
+  if (valor1 === true && valor2 === true) {
+    return true;
+  } else {
+    return false;
+  }  
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+
 }
 
 // Desafio 3
