cbc2e1507fdfd49ed99b508d6752844f7d58d29e
Anderson Leite <andersonleite.dev@gmail.com>
2021-07-02 08:09:08 -0300

    Desafio #10 - Funçao techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,26 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+/**
+ * Consultei a documentação do w3schools para compreender como funciona o metodo
+ * de ordenação 'sort()' na qual eu o utilizei na linha 14 para ordenar 'arrayTecnologies'
+ * Link: https://www.w3schools.com/js/js_array_sort.asp
+ */
+function techList(arrayTecnologies, name) {
+  const tecnologies = [];
+  const namePerson = name;
+  let objectTecnology;
+
+  if (arrayTecnologies.length === 0) return 'Vazio!';
+
+  arrayTecnologies = arrayTecnologies.sort();
+  for (let i = 0; i < arrayTecnologies.length; i += 1) {
+    objectTecnology = {
+      tech: arrayTecnologies[i],
+      name: namePerson,
+    };
+
+    tecnologies.push(objectTecnology);
+  }
+  return tecnologies;
 }
 
 // Desafio 11
