6c2ec911ec3f297913cacf3841a2b8e84aa416cd
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-07-02 14:53:03 -0300

    Exercicio 10 finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,15 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techArray, name) {
+  techArray.sort();
+  let techList = []; // Criei um array pra receber os vários objetos, podendo assim usar o .push
+  if (techArray.length > 0){
+    for (let i of techArray){
+      techList.push({ tech: i, name: name }); // push só funciona para arrays, não objetos.
+    }
+  } else {
+    return 'Vazio!'
+  } 
+  return techList;
 }
 
 // Desafio 11
