c477edaf6d90f189f3cfcc4d54aaaf25c323308a
Vitor Silva <vitor.o.silva95@gmail.com>
2021-07-02 18:35:53 -0300

    Requisito #9

--- src/challenges.js
+++ src/challenges.js
@@ -21,7 +21,6 @@ function splitSentence(sentence) {
 
 // Desafio 4
 function concatName(name) {
-
   let firtsName = name[0];
   let lastName = name[name.length - 1];
   let space = ', ';
@@ -40,7 +39,7 @@ function footballPoints(wins, ties) {
 function highestCount(arrayNumbers) {
   let higherNumber = Math.max(...arrayNumbers);
   let totalNumber = 0;
-  for (let index = 0; index < arrayNumbers.length; index++) {
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
     if (arrayNumbers[index] === higherNumber) {
       totalNumber += 1;
     }
@@ -65,7 +64,7 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(numbers) {
  let fizzBuzzBug = [];
- for (let index = 0; index < numbers.length; index++) {
+ for (let index = 0; index < numbers.length; index += 1) {
    if (numbers[index] % 3 === 0 && numbers[index] % 5 !==0) {
      fizzBuzzBug[index] = "fizz";
    }else if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
@@ -80,11 +79,58 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+function encode(text) {
+  let lettersInNumbers = '';
+    for (let index = 0; index < text.length; index += 1) {
+      switch (text[index]) {
+        case 'a':
+         lettersInNumbers += '1'
+         break;
+        case 'e':
+         lettersInNumbers += '2'
+         break;
+        case 'i':
+         lettersInNumbers += '3'
+         break;
+        case 'o':
+         lettersInNumbers += '4'
+         break;
+        case 'u':
+         lettersInNumbers += '5'
+         break;
+        default:
+         lettersInNumbers += text[index];
+        break;
+      }
+      }
+      return lettersInNumbers;
+    }
+function decode(text) {
+  let numbersInLetters = '';
+
+  for (let index = 0; index < text.length; index += 1) {
+    switch (text[index]) {
+      case '1':
+       numbersInLetters += 'a'
+       break;
+      case '2':
+       numbersInLetters += 'e'
+       break;
+      case '3':
+       numbersInLetters += 'i'
+       break;
+      case '4':
+       numbersInLetters += 'o'
+       break;
+      case '5':
+       numbersInLetters+= 'u'
+       break;
+      default:
+       numbersInLetters += text[index];
+       break;
+    }
+    }
+    return numbersInLetters;
 }
 
 module.exports = {
