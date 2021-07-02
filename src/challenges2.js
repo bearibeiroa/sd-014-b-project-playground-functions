01e18132ca4ce461027a78881d7de29cac247ebc
dhenycruz <dheniarley.ds@gmail.com>
2021-07-02 03:50:09 -0300

    Requisito #11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,7 +10,7 @@ function techList(arrayTech, name) {
       'name': name
     };
   }
-
+  
   return arrayTech.sort(function (a, b) {
     if (a.tech > b.tech) {
       return 1;
@@ -24,8 +24,39 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumber) {
+  // seu código aqui [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
+  let stringNumber = '(';
+  if (arrayNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    let cont = 0;
+    for (let index2 = 0; index2 < arrayNumber.length; index2 += 1) {
+      if (arrayNumber[index] === arrayNumber[index2]){
+        cont += 1;
+      }
+      if(cont >= 3){
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    if (arrayNumber[index] < 0 || arrayNumber[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    if (index === 2) {
+      stringNumber += ') ';
+    }
+    if (index === 7) {
+      stringNumber += '-';
+    }
+    stringNumber += arrayNumber[index];
+  }
+
+  return stringNumber;
 }
 
 // Desafio 12
