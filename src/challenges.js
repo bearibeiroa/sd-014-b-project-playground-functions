f8ec415d87a94afe5377d516385a70320e295a88
Mayara Andronico <mayandronico@msn.com>
2021-07-03 04:48:37 -0300

    altera

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,15 @@
 // Desafio 1
 function compareTrue(a1,a2) {
   // seu código aqui
-  if (a1 === true && a2 === true) {
+  if(a1 === true && a2 === true) {
   return true;
   } else {
-  return false;
-}}
+     return false;
+     }
+    }
   
 // Desafio 2
-function calcArea(base,height) {
+function calcArea(base , height) {
   // seu código aqui
   let triangulo = (base * height) /2;
   return triangulo;
@@ -19,16 +20,16 @@ function splitSentence(frase) {
   // seu código aqui
 return frase.split(" ");
 }
-//console.log(splitSentence('Go trybe'));
+// console.log(splitSentence('Go trybe'));
 
 // Desafio 4
 function concatName(listnames) {
   // seu código aqui
-  let listaConcat = (listnames[0] +" , "+ listnames[listnames.length -1]);
+  let listaConcat = (listnames[listnames.length - 1] + " , " + listnames[0]);
   return listaConcat;
 }
-let listanomes = ['Lucas' , 'Cassiano' , 'Ferraz' , 'Paolillo']
-//console.log(concatName(listanomes));
+let listnames = ['Lucas' , 'Cassiano' , 'Ferraz' , 'Paolillo']
+console.log(concatName(listnames));
 
 // Desafio 5
 function footballPoints(wins , ties) {
@@ -44,20 +45,20 @@ function highestCount(listaNumeros) {
   let maior = 0;
 
   for (let index = 0; index < listaNumeros.length; index += 1) {
-    if(listaNumeros[index] > maior){
+    if(listaNumeros[index] > maior) {
     maior = listaNumeros[index];
     }
   }  
 let count = 0;
-  for (let index = 0; index < listaNumeros.length; index = index + 1){
-    if(listaNumeros[index] === maior){
-    count +=1;
+  for (let index = 0; index < listaNumeros.length; index = index + 1) {
+    if(listaNumeros[index] === maior) {
+    count += 1;
     }
   }
-  console.log("o maior número é:", maior);
-  console.log("quantas vezes aparece" , count);
+  console.log("o maior número é:",maior);
+  console.log("quantas vezes aparece",count);
 }
-  highestCount([4 , 8 , 9 , 9 , 9 , 0]);
+highestCount([4 , 8 , 9 , 9 , 9 , 0]);
 
 // Desafio 7
 function catAndMouse(mouse, cat1 , cat2) {
@@ -66,17 +67,17 @@ function catAndMouse(mouse, cat1 , cat2) {
   let calcDistance2 = (mouse - cat2);
 
   if(calcDistance1 > calcDistance2) {
-    //console.log("o gato mais próximo é: cat 1")
+  // console.log("o gato mais próximo é: cat 1")
      return("cat1 está mais próximo")
     }
 
   if(calcDistance1 < calcDistance2) {
-    //console.log("o gato mais próximo é: cat 2")
+  // console.log("o gato mais próximo é: cat 2")
     return("cat2 está mais próximo")
     }
  
  if (mouse === cat1 && mouse === cat2) {
-    //console.log("os gatos trombam e o rato foge")
+  // console.log("os gatos trombam e o rato foge")
     return("os gatos trombam e o rato foge");
     }
   }
@@ -91,20 +92,20 @@ function fizzBuzz() {
 function encode(codigo) {
   // seu código aqui
 let mensagem = " ";
-for (index = 0; index <= codigo.length; index += 1){
-  if(codigo[index] === "a"){
+for (let index = 0; index <= codigo.length; index += 1) {
+  if (codigo[index] === "a") {
     mensagem = "1";
   }
-    if(codigo[index] === "e"){
+    if (codigo[index] === "e") {
       mensagem = "2";
     }
-    if(codigo[index] === "i"){
+    if (codigo[index] === "i") {
       mensagem = "3";
     }
-    if(codigo[index] === "o"){
+    if (codigo[index] === "o") {
       mensagem = "4";
     }
-    if(codigo[index] === "u"){
+    if (codigo[index] === "u") {
       mensagem = "5";
     }
   }
@@ -115,20 +116,20 @@ console.log(encode("hi there!"));
 function decode(descodigo) {
   // seu código aqui
 
-  for (index = 0; index <= descodigo.length; index += 1){
-    if(descodigo[index] === "1"){
+  for (let index = 0; index <= descodigo.length; index += 1) {
+          if (descodigo[index] === "1") {
       descodigo = "a";
     }
-      if(descodigo[index] === "2"){
+          if (descodigo[index] === "2") {
         descodigo = "e";
       }
-      if(descodigo[index] === "3"){
+          if (descodigo[index] === "3") {
         descodigo = "i";
       }
-      if(descodigo[index] === "4"){
+          if (descodigo[index] === "4") {
         descodigo = "o";
       }
-      if(descodigo[index] === "5"){
+          if (descodigo[index] === "5") {
         descodigo = "u";
   }
 return descodigo;
