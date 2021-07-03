9419c9642ef7f008b7ef2d8f77b638c093ba2021
Danilo dos Santos <danilo.santossouza@outlook.com>
2021-07-03 19:23:59 -0300

    Requisito 10 resolvido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,20 @@
 // Desafio 10
-function techList() {
+function techList(tech, nome) {
   // seu código aqui
+  let arrayTech = [];
+  tech.sort();
+  for (let index = 0; index < tech.length; index += 1){
+    arrayTech.push({
+    tech: tech[index],
+    name: nome })
+  }
+  if (tech.length === 0){
+    arrayTech = "Vazio!"
+  }
+  return arrayTech; 
 }
-
+// let tech = techList(["React", "HTML"], "Lucas");
+// console.log(tech);
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
