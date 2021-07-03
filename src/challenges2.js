aeca2ab43a658a3081018460b4e817103c3a3e08
lucas <mr.pine97@gmail.com>
2021-07-03 16:58:34 -0300

    Requesito #11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,8 +19,42 @@ function techList(techArray, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(theArray) {
+  let count = 0;
+  let repetition = 0;
+  if (theArray.length !== 11) {
+    return "Array com tamanho incorreto."
+  } else {
+    theArray.unshift('(');
+    theArray.splice(3,0,')');
+    theArray.splice(4,0,' ');
+    theArray.splice(10,0,'-');
+  }
+  for (let index = 0; index < theArray.length; index += 1) {
+    if (theArray[index] < 0) {
+      count += 1
+    }
+    else if (theArray[index] > 9) {
+      count += 1
+    }
+    else {
+    let arrayIndex = theArray[index]
+    for (let newIndex = 0; newIndex < theArray.length; newIndex += 1) {
+      let arrayNumber = theArray[newIndex]
+      if (arrayIndex === arrayNumber) {
+        repetition += 1
+      }
+    }
+    }
+  }
+  if (count > 0) {
+    return "não é possível gerar um número de telefone com esses valores"
+  } else if (repetition > 21) {
+    return "não é possível gerar um número de telefone com esses valores"
+  }
+  else {
+    return theArray.join('')
+  }
 }
 
 // Desafio 12
