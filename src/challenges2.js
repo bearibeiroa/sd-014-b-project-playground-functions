2bbaef3cfe1ad818d1ca2939b5cf655824a7aff7
Eliana Nunes <elianadnunes@gmail.com>
2021-07-03 19:42:45 -0300

    Requisito #10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,26 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(technologies,name) {
+  
+  let result = [];
+  technologies.sort();
 
+  if (technologies ==! 0) {
+
+    for (let index = 0; index < technologies.length; index +=1) {
+
+      let object = {
+       tech: ' ',
+       name: name
+      }
+      object.tech = technologies[index];
+      result.push(object);
+    }
+  return result;
+  } else {
+  return 'Vazio!';
+  }
+return techList;
+}
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
