dc43999e50130b13d9a58f96c0952a79344f59c8
Mayara Andronico <mayandronico@msn.com>
2021-07-03 16:09:43 -0300

    reqs

--- src/challenges.js
+++ src/challenges.js
@@ -25,11 +25,11 @@ return frase.split(" ");
 // Desafio 4
 function concatName(listnames) {
   // seu código aqui
-  let listaConcat = (listnames[listnames.length - 1] + " , " + listnames[0]);
+  let listaConcat = (listnames[listnames.length - 1] + ' , ' + listnames[0]);
   return listaConcat;
 }
-let listnames = ['Lucas' , 'Cassiano' , 'Ferraz' , 'Paolillo']
-console.log(concatName(listnames));
+let listnames = (['Lucas' , 'Cassiano' , 'Ferraz' , 'Paolillo'])
+//console.log(concatName(listnames));
 
 // Desafio 5
 function footballPoints(wins , ties) {
@@ -43,7 +43,6 @@ function footballPoints(wins , ties) {
 function highestCount(listaNumeros) {
   // seu código aqui
   let maior = 0;
-
   for (let index = 0; index < listaNumeros.length; index += 1) {
     if(listaNumeros[index] > maior) {
     maior = listaNumeros[index];
@@ -55,8 +54,8 @@ let count = 0;
     count += 1;
     }
   }
-  console.log("o maior número é:",maior);
-  console.log("quantas vezes aparece",count);
+  console.log('o maior número é:',maior);
+  console.log('quantas vezes aparece',count);
 }
 highestCount([4 , 8 , 9 , 9 , 9 , 0]);
 
@@ -68,17 +67,17 @@ function catAndMouse(mouse, cat1 , cat2) {
 
   if(calcDistance1 > calcDistance2) {
   // console.log("o gato mais próximo é: cat 1")
-     return("cat1 está mais próximo")
+     return('cat1 está mais próximo')
     }
 
   if(calcDistance1 < calcDistance2) {
   // console.log("o gato mais próximo é: cat 2")
-    return("cat2 está mais próximo")
+    return('cat2 está mais próximo')
     }
  
  if (mouse === cat1 && mouse === cat2) {
   // console.log("os gatos trombam e o rato foge")
-    return("os gatos trombam e o rato foge");
+    return('os gatos trombam e o rato foge');
     }
   }
   catAndMouse(10 , 8 , 3);
@@ -91,51 +90,51 @@ function fizzBuzz() {
 // Desafio 9
 function encode(codigo) {
   // seu código aqui
-let mensagem = " ";
+let mensagem = ' ' ;
 for (let index = 0; index <= codigo.length; index += 1) {
-  if (codigo[index] === "a") {
-    mensagem = "1";
+  if (codigo[index] === 'a') {
+    mensagem = '1';
   }
-    if (codigo[index] === "e") {
-      mensagem = "2";
+    if (codigo[index] === 'e') {
+      mensagem = '2';
     }
-    if (codigo[index] === "i") {
-      mensagem = "3";
+    if (codigo[index] === 'i') {
+      mensagem = '3';
     }
-    if (codigo[index] === "o") {
-      mensagem = "4";
+    if (codigo[index] === 'o') {
+      mensagem = '4';
     }
-    if (codigo[index] === "u") {
-      mensagem = "5";
+    if (codigo[index] === 'u') {
+      mensagem = '5';
     }
   }
   return codigo;
 }
-console.log(encode("hi there!"));
+console.log(encode('hi there!'));
 
 function decode(descodigo) {
   // seu código aqui
 
   for (let index = 0; index <= descodigo.length; index += 1) {
-          if (descodigo[index] === "1") {
-      descodigo = "a";
+          if (descodigo[index] === '1') {
+      descodigo = 'a';
     }
-          if (descodigo[index] === "2") {
-        descodigo = "e";
+          if (descodigo[index] === '2') {
+        descodigo = 'e';
       }
-          if (descodigo[index] === "3") {
-        descodigo = "i";
+          if (descodigo[index] === '3') {
+        descodigo = 'i';
       }
-          if (descodigo[index] === "4") {
-        descodigo = "o";
+          if (descodigo[index] === '4') {
+        descodigo = 'o';
       }
-          if (descodigo[index] === "5") {
-        descodigo = "u";
+          if (descodigo[index] === '5') {
+        descodigo = 'u';
   }
 return descodigo;
     }
        }
-console.log(decode("h3 th2r2!"));
+console.log(decode('h3 th2r2!'));
 
 module.exports = {
   calcArea,
