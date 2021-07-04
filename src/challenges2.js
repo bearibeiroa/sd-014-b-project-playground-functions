575c8e83a2c33ded2cf6c510e9a9c9c787fc8251
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-07-04 14:32:06 -0400

    add desafio #11

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,8 +23,38 @@ function techList(array, name)
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) 
+{
+  let result;
+  let contador = 2;
+  sortedArray = array.slice().sort();
+
+  if (array.length != 11) return "Array com tamanho incorreto.";
+  
+  for (let i = 0; i < 11; i++)
+  {
+    if (0 > array[i] || array[i]> 9) return "não é possível gerar um número de telefone com esses valores";   
+  };
+  
+  for (let i = 0; i < 9; i++)
+  {
+    if (sortedArray[i] == sortedArray[i+1] && sortedArray[i+1] == sortedArray[i+2]) return "não é possível gerar um número de telefone com esses valores";
+  }
+  
+  result = "(" + array[0] + array[1] + ") ";
+  
+  while (contador < 7)
+  {
+    result = result + array[contador];
+    contador++;
+  }
+  result = result + "-";
+  while (contador < 11)
+  {
+    result = result + array[contador];
+    contador++;
+  }
+  return result;
 }
 
 // Desafio 12
