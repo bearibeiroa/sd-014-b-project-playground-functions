42509ab0293311c001662808496fca540475fc1a
Vanim <vanimouzad123@gmail.com>
2021-07-04 18:18:55 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,37 @@
 // Desafio 10
 function techList(techs, name) {
-  let objectList = [];
-  let techsAndNames = {};
-
-  for (let i = 0; i < techs.length; i += 1) {
-    techsAndNames['']
-  }
+   
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+
+  if (array.length != 11) {
+    return "Array com tamanho incorreto.";
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+
+    if (array[i] < 0 || array [i] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    let contador = 0
+
+    for (let number of array) { //verifica se o número atual de array[i] vai se repetir 3x ou mais.
+      if (array[i] == number) {
+        contador += 1;
+      }
+      if (contador >= 3) {
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+    }
+  }
+        
+  return '(' + array[0] + array[1] + ')' + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
 }
 
 // Desafio 12
