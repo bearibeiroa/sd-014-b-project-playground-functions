2359afcf44d3b839137be703f07d7c7d0ac3a1f4
Henrique Prim <henrique_salmprim@hotmail.com>
2021-07-04 18:22:48 -0300

    Requisito #3 e #4 fixed

--- src/challenges.js
+++ src/challenges.js
@@ -14,21 +14,21 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(str) {
-  let result = str.split('');
+  let result = str.split(' ');
   return result;
 }
 
 // Desafio 4
 function concatName(array) {
   let firstElement = array[0];
-  let lastElement = array[array.lenght - 1];
-  let result = firstElement + lastElement;
+  let lastElement = array[array.length - 1];
+  let result = firstElement + ',' + lastElement;
   return result;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let result = wins *3 + ties;
+  let result = wins * 3 + ties;
   return result;
 }
 
