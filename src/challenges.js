d0b78b8a70c7b94a1a7004d4a8869da45382e41f
FelipeMartins <mfelipeluiz99@gmail.com>
2021-07-04 01:29:18 -0300

    Requsito #6 e #7

--- src/challenges.js
+++ src/challenges.js
@@ -24,6 +24,7 @@ function concatName(arrayC) {
   let stringC = arrayC[arrayC.length - 1] + ', ' + arrayC[0];
   return stringC;
 }
+
 // Desafio 5
 function footballPoints(wins, ties) {
   let pontos = wins * 3 + ties;
@@ -31,13 +32,43 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(n) {
+  var maior = n[0];
+  var maiorNumero = [];
+  for (i = 0; i < n.length; i++) {
+    if (n[i] >= maior) {
+      maior = n[i];
+    }
+  }
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] == maior) {
+      maiorNumero.push(n[i]);
+    }
+  }
+  return maiorNumero.length;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  var gato1 = 0;
+  var gato2 = 0;
+  if (mouse >= cat1) {
+    gato1 = mouse - cat1;
+  } else {
+    gato1 = cat1 - mouse;
+  }
+  if (mouse >= cat2) {
+    gato2 = mouse - cat2;
+  } else {
+    gato2 = cat2 - mouse;
+  }
+  if (gato1 < gato2) {
+    return 'cat1';
+  } else if (gato2 < gato1) {
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
