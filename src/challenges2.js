0538281b9ec500463e4027f45d04a624b7e5fa24
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-04 16:02:14 -0300

    Alterações ESLint.

--- src/challenges2.js
+++ src/challenges2.js
@@ -54,8 +54,6 @@ for (let n in numbers){
           let phoneNumber = ('(' + numbers[0] + numbers[1] +') ' + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + '-' + numbers[7] + numbers[8] + numbers[9] + numbers[10]);
           return phoneNumber;
         }
-    
-  
 }
 
 // Desafio 12
