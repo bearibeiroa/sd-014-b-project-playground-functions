9df5d7a23355617a789ec753e7a0c10dc484eff2
Elithon Silva <elithonsilva@gmail.com>
2021-07-04 19:08:30 -0300

    Requisito-13

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,7 +5,7 @@ function techList(tech, name) {
   // seu código aqui
   let studyList = [];
   if (tech.length === 0) {
-    return "Vazio!";
+    return 'Vazio!';
   }
   for (let index of tech.sort()) {
     studyList.push({
@@ -22,17 +22,18 @@ function techList(tech, name) {
 function generatePhoneNumber(numbers) {
   // seu código aqui
   if (numbers.length !== 11) {
-    return "Array com tamanho incorreto.";
+    return 'Array com tamanho incorreto.';
   }
+  let contador = 0;
   for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] < 0 || numbers[index] > 9) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
   let ddd = [numbers[0], numbers[1]];
   let contactPart1 = [numbers[2], numbers[3], numbers[4], numbers[5], numbers[6]];
   let contactPart2 = [numbers[7], numbers[8], numbers[9], numbers[10]];
-  return "(" + ddd.join('') + ") " + contactPart1.join('') + '-' + contactPart2.join('');
+  return '(' + ddd.join('') + ') ' + contactPart1.join('') + '-' + contactPart2.join('');
 }
 
 console.log(generatePhoneNumber([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
@@ -51,8 +52,24 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
+// String.prototype.replace() - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replace - Usei o replace para extrair somente os numeros da string.
+// split() - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split - Com o Split inseri os numeros capturados numa string inteira. Como separator usei uma string vazia, isso faz com que a string inteira seja convertida em um array de caracteres.
+// Array.prototype.map() https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map - Na documentação, em "Exemplo: Caso de uso mais complexo", usei o último exemplo da lista (nomeado como Bonus), onde é sujerido usar .map(Number) para remover as aspas dos elementos no array.
+// Recebi auxilio de Écio Ferraz - Turma 14 - Tribo A. Ele me deu uma luz indicando possíveis ferramentas que eu poderia usar para chegar ao resultado. Todas as funções pesquisadas estão referênciadas acima.
+
+function hydrate(text) {
   // seu código aqui
+  let stringNumbers = text.replace(/[^0-9]/g, '');
+  let numbers = stringNumbers.split('').map(Number);
+  let soma = 0;
+  for (index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+  }
+  if (soma === 1) {
+    return soma + ' copo de água';
+  } else {
+    return soma + ' copos de água';
+  }
 }
 
 module.exports = {
