554320f6ded122da4eab810a83dd3aaebf09dd2b
Paola Gay <paolagaydossantos@gmail.com>
2021-07-04 21:48:57 -0300

    Correçao erros - F8 e espaçamento de outras Fs

--- src/challenges.js
+++ src/challenges.js
@@ -15,26 +15,26 @@ function splitSentence(Value) {
 
 // Desafio 4
 function concatName(arrayIn) {
-arrayIn.splice(0,0, arrayIn[arrayIn.length-1]);
-  arrayIn.splice(2,arrayIn.length-2);
+  arrayIn.splice(0, 0, arrayIn[arrayIn.length -1]);
+  arrayIn.splice(2, arrayIn.length -2);
   return arrayIn.join(', ');
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
- return ((3 * wins) + ties);
+  return ((3 * wins) + ties);
 }
 
 // Desafio 6
 function highestCount(parameter) {
   let cont = 0; 
   let max = Math.max.apply(null,parameter);
-    for (let index = 0; index < parameter.length; index += 1){
+  for (let index = 0; index < parameter.length; index += 1) {
     if (parameter[index] === max) {
-    cont = cont + 1; 
-      }
+      cont = cont + 1; 
     }
-    return (cont);
+  }
+  return (cont);
 }
   
 // Desafio 7
@@ -42,34 +42,34 @@ function catAndMouse(mouse, cat1, cat2) {
   cont1 = 0;
   cont2 = 0;
 
-   while (mouse != cat1){
-   if (mouse > cat1 ){
-   cat1 = cat1 + 1  
-   cont1 = cont1 +1
-   } else {
-     cat1 = cat1 - 1
-     cont1 = cont1 + 1
-   }
+  while (mouse !== cat1) {
+    if (mouse > cat1 ){
+      cat1 = cat1 + 1;
+      cont1 = cont1 + 1;
+    } else {
+      cat1 = cat1 - 1;
+      cont1 = cont1 + 1;
+  }
   }
 
-    while (mouse != cat2){
-      if (mouse > cat2 ){
-        cat2 = cat2 + 1  
-       cont2 = cont2 +1
-        } else {
-          cat2 = cat2 - 1
-         cont2 = cont2 + 1
-         }  
+  while (mouse !== cat2) {
+    if (mouse > cat2) {
+      cat2 = cat2 + 1;  
+      cont2 = cont2 + 1;
+    } else {
+      cat2 = cat2 - 1;
+      cont2 = cont2 + 1;
+    }  
    }
-   let string = 0;
-   if (cont1 === cont2){
-       string = 'os gatos trombam e o rato foge';
-   } else if (cont1 > cont2){
-       string = 'cat2'
-   } else {
-     string = 'cat1'
-   }
-     return string; 
+  let string = 0;
+  if (cont1 === cont2) {
+    string = 'os gatos trombam e o rato foge';
+  } else if (cont1 > cont2) {
+    string = 'cat2';
+  } else {
+    string = 'cat1';
+  }
+  return string; 
 }
 
 // Desafio 8
