b2e556f551aadf44a8d1029b96db2ba81eba1ed4
Nasser <nasseralmeida@gmail.com>
2021-07-05 19:30:09 -0300

    Commitinho Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techList, name) {
+  let arrayOfObjects = [];
+  techList.sort();
+  if (techList.length ===0) {
+    return 'Vazio!';
+  }
+
+  for (index = 0; index < techList.length; index += 1) {
+    let objectsList = {
+      tech: '',
+      name: name,
+    };
+    objectsList.tech = techList[index];
+    arrayOfObjects.push(objectsList);
+  }
+  return arrayOfObjects;
 }
+
 
 // Desafio 11
 function generatePhoneNumber() {
