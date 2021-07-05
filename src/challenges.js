26ca0bb2edea646c58f6eb048a51d8fea3647d67
dhenycruz <dheniarley.ds@gmail.com>
2021-07-05 00:32:11 -0300

    Refatorando o códgio e aplicando algumas referências.

--- src/challenges.js
+++ src/challenges.js
@@ -20,7 +20,7 @@ function splitSentence(string) {
   // seu código aqui
   /**
    * Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
-   * Utilizei a function split() passando o parâmetro ' ' para separa a string e a própria split() separa em array. 
+   * Utilizei a function split() passando o parâmetro ' ' para separa a string e a própria split() separa em array.
    */
   let arrayString = string.split(' ');
 
@@ -31,7 +31,11 @@ function splitSentence(string) {
 function concatName(arrayString) {
   // seu código aqui
   let index = arrayString.length - 1;
-  let resultString = arrayString[index] +', '+arrayString[0];
+  /**
+   * Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Template_literals
+   * Coloquei as variáveis dentro do acento grave, para poder manipular o array como string.
+   */
+  let resultString = `${arrayString[index]}, ${arrayString[0]}`;
 
   return resultString;
 }
@@ -67,95 +71,91 @@ function highestCount(numbers) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if (cat1 > mouse) {
-    cat1 -= mouse;
+  let posCat1 = Math.abs(cat1 - mouse);
+  let posCat2 = Math.abs(cat2 - mouse);
+  let result;
+
+  if (posCat1 > posCat2) {
+    result = 'cat2';
+  } else if (posCat1 < posCat2) {
+    result = 'cat1';
   } else {
-    cat1 = mouse - cat1;
+    result = 'os gatos trombam e o rato foge';
   }
 
-  if (cat2 > mouse) {
-    cat2 -= mouse;
-  } else {
-    cat2 = mouse - cat2;
+  return result;
+}
+
+// Function para verificar se o item do array é fizzBuzz, bug!, fizz ou buzz
+function checkItem(item) {
+  /**
+   * Para cada número do Array que seja divisível apenas por 3, apresente uma string "fizz";
+   * Para cada número do Array que seja divisível apenas por 5, apresente uma string "buzz";
+   * Caso o número seja divisível por 3 e 5, retorne a string "fizzBuzz";
+   * Caso o número não possa ser dividido por 3 nem por 5, retorne a string "bug!";
+   */
+  if (item % 3 === 0 && item % 5 === 0) {
+    return 'fizzBuzz';
   }
-  if (cat1 > cat2) {
-    return 'cat2';
-  } else if (cat1 < cat2) {
-    return 'cat1';
-  } else {
-    return 'os gatos trombam e o rato foge';
+  if (item % 3 === 0) {
+    return 'fizz';
   }
+  if (item % 5 === 0) {
+    return 'buzz';
+  }
+
+  return 'bug!';
 }
 
 // Desafio 8
-function fizzBuzz(arrayNumber) {
+function fizzBuzz(arrayNumber = [7, 9]) {
   // seu código aqui
   for (let index = 0; index < arrayNumber.length; index += 1) {
-    if (arrayNumber[index] % 3 === 0 && arrayNumber[index] % 5 === 0) {
-      arrayNumber[index] = 'fizzBuzz';
-    } else if (arrayNumber[index] % 3 !== 0 && arrayNumber[index] % 5 !== 0) {
-      arrayNumber[index] = 'bug!';
-    } else if (arrayNumber[index] % 3 === 0) {
-      arrayNumber[index] = 'fizz';
-    } else {
-      arrayNumber[index] = 'buzz';
-    }
+    arrayNumber[index] = checkItem(arrayNumber[index]);
   }
-
   return arrayNumber;
+}
+
+// function criada para fazer a troca de vogal para numero e numero para vocal utilizando a function replace
+function changeReplaceEncode(param1, expReg, objSubs) {
+  /**
+   * Sourc: https://blog.betrybe.com/javascript/javascript-replace/
+   * Utilizei a function replace com expressões regulares para trocar todos os caracteres de uma vez sem necessitar usar um for para pecorrer a string e ifs para aplicar as condições.
+   */
+  let alteracaoLista = param1.replace(expReg, function (item) {
+    let it = objSubs[item];
+    let itemLista = it.replace(function (elemento) { return elemento.toUpperCase(); });
+    return itemLista;
+  });
+
+  return alteracaoLista;
 }
 
 // Desafio 9
 function encode(string) {
   // seu código aqui
-  for (let vogal of string) {
-    switch (vogal) {
-      case 'a':
-        string = string.replace('a', '1');
-        break;
-      case 'e':
-        string = string.replace('e', '2');
-        break;
-      case 'i':
-        string = string.replace('i', '3');
-        break;
-      case 'o':
-        string = string.replace('o', '4');
-        break;
-      case 'u':
-        string = string.replace('u', '5');
-        break;
-      default:
-        break;
-    }
-  }
-
-  return string;
+  let expReg = /a|e|i|o|u/gi;
+  let listaSubstituicao = {
+    a: '1',
+    e: '2',
+    i: '3',
+    o: '4',
+    u: '5',
+  };
+  return changeReplaceEncode(string, expReg, listaSubstituicao);
 }
 
 function decode(string) {
   // seu código aqui
-  for (let number of string) {
-    switch (number) {
-     case '1': 
-        string = string.replace('1', 'a');
-        break;
-      case '2':
-        string = string.replace('2', 'e');
-        break;
-      case '3':
-        string = string.replace('3', 'i');
-        break;
-      case '4':
-        string = string.replace('4', 'o');
-        break;
-      case '5':
-        string = string.replace('5', 'u');
-        break;
-    }
-  }
-
-  return string;
+  let expReg = /1|2|3|4|5/gi;
+  let listaSubstituicao = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return changeReplaceEncode(string, expReg, listaSubstituicao);
 }
 
 module.exports = {
