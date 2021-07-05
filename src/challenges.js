86b2f484835e9615e24d9aa2cf9a451551be6e19
enzo <enzo@pop-os.localdomain>
2021-07-05 10:20:38 -0300

    desafio 10

--- src/challenges.js
+++ src/challenges.js
@@ -3,25 +3,30 @@ function compareTrue(value1, value2) {
   if (value1 === true && value2 === true) {
     return true;
   } return false;
-} 
+}
+// Essa função recebe dois parâmetros(entre "true" e "false"), retornando true se ambos forem verdadeiros, e retornando false caso pelo menos um entre esses parâmetros for false.
 
 // Desafio 2
 function calcArea(base, altura) {
   let areaTriangulo = (base * altura) / 2;
   return areaTriangulo;
-};
+}
+// Essa função recebe dois parâmetros (numéricos), relacionados a medida da base e da altura de um triângulo. A função multiplica o valor da base pelo valor da altura e divide o resultado por 2. Após essa operação, ela retorna o valor obtido.
 
 // Desafio 3
+// ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
 function splitSentence(string) {
-  let resultado = string.split(' ');
+  let resultado = string.split(' '); 
   return resultado;
-};
+}
+// Para realizar esse desafio, consultei a documentação do método '.split'. Essa função recebe uma string como parâmetro e retorna um array de strings separadas por espaço. 
 
 // Desafio 4
 function concatName(array) {
   let concate = array[array.length - 1] + ', ' + array[0];
   return concate;
-};
+}
+// Essa função recebe um array como parâmetro. A variável "concate" pega o último elemento do array recebido e concatena com uma vírgula e espaço ", " e o primeiro do elemento do array. Após isso, a função retorna o valor da variável "concat".
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -29,9 +34,12 @@ function footballPoints(wins, ties) {
   let tiePoints = ties;
   let totalPoints = winPoints + tiePoints;
   return totalPoints;
-};
+}
+// Essa função recebe dois parâmetros, o número de vitórias e o número de empates de um time respectivamente. Na primeira variável "winPoints", calcula a quantidade de pontos baseada no número de vitórias de um time multiplicados por 3. Na segunda variável "tiePoints", armazena o número de pontos por empate de um time. E na terceira variável "totalPoints", acontece a soma dos pontos de vitória e de empate. Logo após isso, a função retorna a variável "totalPoints".
 
 // Desafio 6
+// ref : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+// ref : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Function/apply#usando_apply_e_fun%C3%A7%C3%B5es_embutidas
 function highestCount(array) {
   let highNumber = Math.max.apply(null, array);
   let numberCount = 0;
@@ -41,21 +49,21 @@ function highestCount(array) {
     }
   }
   return numberCount;
-};
+} // Essa função recebe um array numérico como parâmetro. Nesse desafio usei o médoto "Math.max.apply()" consultando a documentação no site do MDN. A primeira variável "highNumber" armazena o maior número entre os números recebidos no array. A segunda variávvel "numberCount" é um contador, para conferir quantas vezes o número se repete dentro do array. Usando o 'for' para percorrer as posições do array, se o número da posição do array for igual ao maior número, a variável "numberCount" é incrementada em uma unidade. Após esse processo, a função retorna o valor de "numberCount".  
 
 // Desafio 7
+// ref : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
 function catAndMouse(mouse, cat1, cat2) {
- let rangeCat1 = mouse - cat1;
- let rangeCat2 = mouse - cat2;
-  
- if ( Math.abs(rangeCat1) === Math.abs(rangeCat2)) {
+  let rangeCat1 = mouse - cat1;
+  let rangeCat2 = mouse - cat2;
+  if ( Math.abs(rangeCat1) === Math.abs(rangeCat2)) {
     return 'os gatos trombam e o rato foge';
   } else if (Math.abs(rangeCat1) < Math.abs(rangeCat2)) {
     return 'cat1';
-  } else { (Math.abs(rangeCat2) < Math.abs(rangeCat1));
+  } else {(Math.abs(rangeCat2) < Math.abs(rangeCat1));
     return 'cat2';
-  };
-}; 
+  }
+} // Essa função recebe três parâmetros de posições, do rato, do cat 1 e do cat 2 respectivamente. Nesse desafio usei o método "Math.abs()" para retornar o valor da distância em módulo. A variável "rangeCat1" verifica a distância do 'cat1' em relação ao 'mouse'. A variável "rangeCat2" faz a mesma coisa só que com a distância do 'cat2' em relação ao mouse. Se a distância dos cats 1 e 2 é a mesma em relação ao mouse, a função retorna a string 'os gatos trombam e o rato foge'. Se a distância do cat1 for menor do que a distância do cat2 em relação ao mouse, a função retorna 'cat1'. Caso o contrário aconteça, ela retorna 'cat2'.
  
 // Desafio 8
 function fizzBuzz(array) {
@@ -65,15 +73,17 @@ function fizzBuzz(array) {
     resultado.push('fizzBuzz');
       } else if ((index % 5 === 0)) {
     resultado.push('buzz');
-      } else if ((index % 3 === 0) ){
+      } else if ((index % 3 === 0)) {
     resultado.push('fizz');
       } else {
     resultado.push('bug!');
       };
     } return resultado;  
-};
+}
+// Essa função recebe um array numérico. A variável "resultado" é um array vazio, feito para armazenar dados. Se os números recebidos forem divisíveis por 3 e por 5, utilizando o método '.push', a string 'fizzBuzz' é enviada para a última posição do array "resultado". Se o número recebido for divisível apenas por 5, é armazenada a string 'buzz' no final do array "resultado". Já se o número recebido for divisível apenas por 3, é armazenada a string 'fizz' no final do array "resultado". Se os número recebido não atender à nenhuma das três condições anteriores, é armazenada a string 'bug' no final do array "resultado".
   
 // Desafio 9
+// ref : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/join
 function encode(palavra) {
   let letras = [];
   for (let letra of palavra){
@@ -91,8 +101,8 @@ function encode(palavra) {
       } else if (letras[index] === 'u') 
         letras[index] = '5';
     }
-    return letras.join('');
-  }; console.log(encode('boa noite pessoal'))
+    return letras.join('')
+  } 
       
 function decode(palavra) {
   let numbers = [];
@@ -102,18 +112,22 @@ function decode(palavra) {
   for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] === '1') {
       numbers[index] = 'a';
-    } else if (numbers[index] === '2'){
+    } else if (numbers[index] === '2') {
       numbers[index] = 'e';
-    } else if (numbers[index] === '3'){
-        numbers[index] = 'i';
-    } else if (numbers[index] === '4'){
-        numbers[index] = 'o';
-    } else if (numbers[index] === '5'){
-        numbers[index] = 'u';
-    };
-   };
-   return numbers.join('');
-}; console.log(decode('b41 n43t2 p2ss41l')) .
+    } else if (numbers[index] === '3') {
+      numbers[index] = 'i';
+    } else if (numbers[index] === '4') {
+      numbers[index] = 'o';
+    } else if (numbers[index] === '5') {
+      numbers[index] = 'u';
+    }
+  }
+  return numbers.join('');
+} 
+
+// Nesse deasfio usei o método join(), para juntar os elementos do array "letras" em uma string e retornar a mesma. A função "encode" recebe uma string como parâmetro. A variável "letras" é um array vazio onde os caracteres serão armazenados separadamente. No primeiro 'for' ocorre o acesso de cada caractere da string recebida, armazenando-os no array "letras" com o auxílio do método ".push". No segundo 'for' ocorre a comparação das letras de cada palavra no array "letras". Se a variável de posição (letras[index]) de uma determinada palavra tiver o mesmo valor e o mesmo tipo da string da vogal comparada, acontece a troca pelo número respectivo atrelado à esta vogal nas condicionais da função. Depois a função retorna o array unificado com o auxílio do método ".join".
+// A função decode faz o processo inverso da função decode. Ele compara as palavras codificadas e às decodifica. 
+
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,48 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
-
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
-
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
-
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+function techList(techName, name) {
+  techName.sort();
+  let techVector = [];
+   
+  if (techName.length > 0) {
+     for (let index of techName) {
+       techVector.push({ tech: index, name: name });
+     }
+    } else {
+    return 'Vazio!'
+  }
+ 
+  return techVector;
+ 
+ } console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Enzo'))
+ // A função techList recebe como parâmetros um array e uma string. O '.sort' é utilizado para colocar o array recebido em ordem alfabética. A variável "techVector" é um array vazio, criado para armazenar propiedades da minha lista. Se o tamanho do meu array for maior que zero, com o uso do 'for', os valores do meu array são percorridos em loop, e à cada loop, é armazenado no final do array, com o uso do '.push', uma nova propriedade, contendo o item de uma posição do array e o nome recebido na função. Se o array recebido for vazio, a função retorna "Vazio!". Depois desses processos de verificação, a função retorna o array 'techVector'.
+ 
+ 
+ // Desafio 11
+ function generatePhoneNumber(numbers) {
+   
+   if (numbers.length != 11) {
+     return "Array com tamanho incorreto."
+   }
+   for (let index of numbers) {
+     if (numbers[index] < 0 || numbers[index] > 9) {
+       return "Não é possível gerar um número de telefone com esses valores."
+     }
+   }
+ 
+   
+ 
+ } 
+ 
+ // Desafio 12
+ function triangleCheck(lineA, lineB, lineC) {
+   
+ 
+ }
+ 
+ // Desafio 13
+ function hydrate() {
+   
+ }
 
 module.exports = {
   generatePhoneNumber,
