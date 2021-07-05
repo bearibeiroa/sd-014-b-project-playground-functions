4c3bfb52c7ef812f5abbe615e7b5b7b47a8c17c7
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-05 19:28:24 -0300

    Requesito numero 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,37 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function generatePhoneNumber(foneNumbers) {
+  let cont = 0;
+  let ddd = '';
+  let first = '';
+  let second = '';
+  if (foneNumbers.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < foneNumbers.length; index += 1) {
+    cont = 0;
+    for (let indexTwo = 0; indexTwo < foneNumbers.length; indexTwo += 1) {
+      if (foneNumbers[indexTwo] < 0 || foneNumbers[indexTwo] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      if (foneNumbers[index] === foneNumbers[indexTwo]) {
+        cont += 1;
+      }
+    }
+    if (cont > 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    if (index < 2) {
+      ddd += foneNumbers[index];
+    } else if (index > 1 && index < 7) {
+      first += foneNumbers[index];
+    } else {
+      second += foneNumbers[index];
+    }
+  }
+  return '(' + ddd + ') ' + first + '-' + second;
 }
 
 // Desafio 12
