15787c185cb0c633de4bb2eba168549755c95223
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-07-04 20:06:46 -0400

    Desafio 11 resolvido

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,45 +17,33 @@ console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Humberto")
 
 // Desafio 11
 function generatePhoneNumber(numeros) {
-  var numbersSize = numeros.length;
   let answer = "";
-  if (numbersSize > 11 || numbersSize < 11) {
-    answer =  "Array com tamanho incorreto.";
+  if (numeros.length != 11) {
+    return 'Array com tamanho incorreto.';
   }
-  for (let index in numeros) {
-    if (numeros[index] > 9 || numeros[index] < 0) {
-      answer = "não é possível gerar um número de telefone com esses valores"
-    }
-  }
-  ale = [];
-  els = "";
-  let part = "";
-  let numb = "";
-  for (let index = 0; index < 11; index += 1) {
-    var randomnum = numeros[Math.floor(Math.random() * numeros.length)]
-    ale.push(randomnum)
-    els = ale.join('')
-    // join Fonte: https://metring.com.br/javascript-converter-array-em-string
-    part2 = Math.ceil((els.length / 2))
-    answer = "(" + els.substr(0, 2) + ")" + els.substr(0, part2 - 1) + "-" + els.substr(part2 + 1);
-    // substr Fonte: https://pt.stackoverflow.com/questions/13738/adicionar-caractere-no-meio-de-uma-string
-  }
-  let contNumero = 0;
   for (let indice in numeros) {
-    let verificaNumero = numeros[indice];
-    for (let index2 in numeros) {
-      if (verificaNumero === numeros[index2]) {
-        contNumero += 1;
+    if (numeros[indice] > 9 || numeros[indice] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    } else {
+      let contador = 0;
+      for (let index in numeros) {
+        if (numeros[indice] === numeros[index]) {
+          contador += 1;
+        }
+      }
+      if (contador >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores'
       }
     }
-    if (contNumero >= 3) {
-      answer = "não é possível gerar um número de telefone com esses valores";
-      break;
-    }
   }
+  els = numeros.join('')
+  // join Fonte: https://metring.com.br/javascript-converter-array-em-string
+  part2 = Math.ceil((els.length / 2))
+  answer = "(" + els.substr(0, 2) + ")" + " " + els.substr(2, 5) + "-" + els.substr(part2 + 1);
+  // substr Fonte: https://pt.stackoverflow.com/questions/13738/adicionar-caractere-no-meio-de-uma-string
   return answer;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 0, 6, 7, 8, 9, 0, 2]))
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
