c8d2578bb20b8b6f6595d82bf5a0c5a2724f5c94
natalia <mellomartins.naty@gmail.com>
2021-07-05 18:28:12 -0300

    lint correcoes

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 === true && value2 ===true) {
+  if (value1 === true && value2 === true) {
     return true;
   } else {
     return false;
@@ -9,8 +9,8 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, altura) {
-  return (base * altura / 2);
-  }
+  return ((base * altura) / 2);
+}
 
 // Desafio 3
 function splitSentence(string) {
@@ -19,11 +19,10 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  let ultimoItem = array[array.length-1];
+  let ultimoItem = array[array.length - 1];
   let primeiroItem = array[0];
   let contatenada = [ultimoItem, primeiroItem];
-    return contatenada.join(', ');
-  
+  return contatenada.join(', ');  
 }
 
 // Desafio 5
@@ -35,68 +34,63 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(valores) {
 
-// Função para o highestNumber encontrada na documentação, em https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max e https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce onde
-// math.max seleciona o maior valor entre 2 elementos do array e
-//reduce executa esta função para todos os elementos do array.
-
+  // Função para o highestNumber encontrada na documentação, em https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max e https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce onde
+  // math.max seleciona o maior valor entre 2 elementos do array e
+  //reduce executa esta função para todos os elementos do array.
+  
 let highestNumber = valores.reduce(function(a, b) {
-  return Math.max(a, b);
-});
-let count = 0
+  return Math.max (a, b);
+  });
+  let count = 0
 
 for (let index6 = 0; index6 < valores.length; index6 += 1) {
-  let current = valores[index6];
-  if (current == highestNumber) {
-    count += 1;
-  } else {
-  }
+    let current = valores[index6];
+    if (current == highestNumber) {
+      count += 1;
+    } 
 }
  return count;
 }
   
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let distCat1 = mouse - cat1;
   if (distCat1 <= 0) {
     distCat1 = distCat1 * -1
-    } else {
-    }
+  }
     
   let distCat2 = mouse - cat2;
   if (distCat2 <= 0) {
     distCat2 = distCat2 * -1
-    } else {
-    }
+  }
   console.log(distCat1);
   console.log(distCat2);
 
   if (distCat1 < distCat2) {
-    return "cat1";
+    return 'cat1';
   } else if (distCat2 < distCat1) {
-    return "cat2";
+    return 'cat2';
   } else {
-    return "os gatos trombam e o rato foge";
-  }
-  
+    return 'os gatos trombam e o rato foge';
+  }  
 }
 
 // Desafio 8
 function fizzBuzz(fizzOuBuzz) {
 
-  for (let index8 = 0; index8 < fizzOuBuzz.length; index8 += 1) {
-    if (fizzOuBuzz[index8] % 3 === 0 && fizzOuBuzz[index8] % 5 === 0) {
-      fizzOuBuzz[index8] = 'fizzBuzz';
-    } else if (fizzOuBuzz[index8] % 3 === 0 && fizzOuBuzz[index8] % 5 != 0) {
-      fizzOuBuzz[index8] = 'fizz';
-    } else if (fizzOuBuzz[index8] % 3 != 0 && fizzOuBuzz[index8] % 5 === 0) {
-      fizzOuBuzz[index8] = 'buzz';
-    } else {
-      fizzOuBuzz[index8] = 'bug!';
-    }
+for (let index8 = 0; index8 < fizzOuBuzz.length; index8 += 1) {
+  if (fizzOuBuzz[index8] % 3 === 0 && fizzOuBuzz[index8] % 5 === 0) {
+  fizzOuBuzz[index8] = 'fizzBuzz';
+  } else if (fizzOuBuzz[index8] % 3 === 0 && fizzOuBuzz[index8] % 5 != 0) {
+  fizzOuBuzz[index8] = 'fizz';
+  } else if (fizzOuBuzz[index8] % 3 != 0 && fizzOuBuzz[index8] % 5 === 0) {
+  fizzOuBuzz[index8] = 'buzz';
+  } else {
+  fizzOuBuzz[index8] = 'bug!';
   }
-  return fizzOuBuzz;
 }
+return fizzOuBuzz;
+}  
 
 // Desafio 9
 function encode(codifique) {
