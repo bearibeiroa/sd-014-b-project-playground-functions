93b354e77d9b5baa37d24121c23fa3a714ef1f16
enzo <enzo@pop-os.localdomain>
2021-07-05 21:21:34 -0300

    número de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,20 +19,45 @@ function techList(techName, name) {
  
 
  // Desafio 11
+ function hasElevenNumbers (array){
+  if (array.length != 11) {
+    return true
+  }
+}
+// A utilização de "const element = array[index]" e "array.filter((x) => x === element).length >= 3" foram retirados do repositório do Welton Thomas. Das linhas 49 à 51 e a linha 56 também foram retiradas da mesma fonte.
+// ref : "https://github.com/tryber/sd-014-b-project-playground-functions/pull/70/commits/e6fa301e8300c4546cbc654929dde2238f924fb7" 
+// ref : "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/filter" "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substring" "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/const"
+
+function numberVerifier (array) {
+  for (let index in array) {
+     const element = array[index]
+    if (element < 0 || element > 9 || array.filter((x) => x === element).length >= 3) {
+      return true
+    }
+  }
+} 
+
  function generatePhoneNumber(numbers) {
-   
-   if (numbers.length != 11) {
-     return "Array com tamanho incorreto."
-   }
-   for (let index of numbers) {
-     if (numbers[index] < 0 || numbers[index] > 9) {
-       return "Não é possível gerar um número de telefone com esses valores."
-     }
-   }
- 
-   
- 
- } 
+  if (hasElevenNumbers(numbers) === true) {
+    return "Array com tamanho incorreto."
+  }
+  
+  if (numberVerifier(numbers) === true) {
+    return "não é possível gerar um número de telefone com esses valores"
+  }
+  let phoneNumber = '';
+  numbers.forEach((element) => { 
+    phoneNumber += element;
+  });
+  
+  let ddd = phoneNumber.substring(0,2);
+  let firstFive = phoneNumber.substring(2,7);
+  let lastFour = phoneNumber.substring(7);
+  phoneNumber = `(${ddd}) ${firstFive}-${lastFour}`;
+  return phoneNumber;
+  } 
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
  
  // Desafio 12
  // Para resolver esse desafio, usei a lógica e a dica do Thomas Ferreira no Slack. ref : "https://github.com/tryber/sd-014-b-project-playground-functions/pull/90https://github.com/tryber/sd-014-b-project-playground-functions/pull/90"
@@ -41,10 +66,10 @@ function techList(techName, name) {
    let diffAB = Math.abs(lineA - lineB)
    let diffAC = Math.abs(lineA - lineC)
    let diffBC = Math.abs(lineB - lineC)
-   let sizeA = lineA < lineB + lineC && lineA > diffBC
-   let sizeB = lineB < lineA + lineC && lineB > diffAC
-   let sizeC = lineC < lineA + lineB && lineC > diffAB
-   let triangle = sizeA + sizeB + sizeC 
+   let ladoA = lineA < lineB + lineC && lineA > diffBC
+   let ladoB = lineB < lineA + lineC && lineB > diffAC
+   let ladoC = lineC < lineA + lineB && lineC > diffAB
+   let triangle = ladoA + ladoB + ladoC 
     return !!triangle;
   
  } 
