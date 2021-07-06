d66b6f76abd9d84e81a1734293aa148db821f3fa
S4bot4 <sabotamail@gmail.com>
2021-07-06 03:38:54 -0300

    Cumprido requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,20 +3,38 @@ function techList(tech, name) {
   // criar um array vazio
   // criar um objeto vazio
   // o for vai jogar dentro do objeto
-     tech.sort();
-     let listOfTechs = [];
+  tech.sort();
+  let listOfTechs = [];
   if (tech.length === 0) {
     return 'Vazio!';
   }
   for (let index of tech) {
     listOfTechs.push({'tech':index, 'name':name});
-    
   }
   return listOfTechs;
 }
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(number) {
   // seu código aqui
+ 
+  if (number.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index of number) {
+    if (number[index] < 0 || number[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  for (let num of number) {
+    let counter = 0;
+    for (indexcounter of number) {
+      if (num === indexcounter) {
+        counter += 1;
+      } if (counter >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      } 
+    }
+  } return '('+ number[0] + number[1] + ') ' + number[2]  + number[3]  + number[4]  + number[5]  + number[6]  +'-' + number[7]  + number[8]  + number[9]  + number[10];
 }
 
 // Desafio 12
