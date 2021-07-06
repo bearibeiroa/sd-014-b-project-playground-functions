bd5854a26a3e28f250b548ff1272311f78ed0e98
bruholanda <brunaholandacunha7@gmail.com>
2021-07-06 08:58:40 -0300

    Indentation changes;

--- src/challenges.js
+++ src/challenges.js
@@ -10,12 +10,12 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  return (string.split(" "));
+  return (string.split(' '));
 }
 
 // Desafio 4
 function concatName(array) {
-  var concat = array[array.length - 1];
+  let concat = array[array.length - 1];
   concat += ', ';
   concat += array[0];
   return concat;
@@ -28,11 +28,12 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(numbers) {
-  var maior, cont = 0;
+  let maior;
+  let cont = 0;
   maior = Math.max(...numbers);
 
   for (const key in numbers) {
-    if (maior == numbers[key]){
+    if (maior === numbers[key]) {
       cont++;
     }
   }
@@ -53,7 +54,7 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8
 function fizzBuzz(numbers) {
-  var array = [];
+  let array = [];
   for (const key in numbers) {
     if ((numbers[key] % 5 === 0) && (numbers[key] % 3 === 0)) {
       array.push('fizzBuzz');
@@ -70,9 +71,9 @@ function fizzBuzz(numbers) {
 
 // Desafio 9
 function encode(string) {
-  var splited = string.split('');
+  let splited = string.split('');
 
-  for (let key in splited) {
+  for (const key in splited) {
     switch (splited[key]) {
       case 'a':
         splited[key] = '1';
@@ -87,9 +88,8 @@ function encode(string) {
         splited[key] = '4';
         break;
       case 'u':
-        splited[key] = "5";
+        splited[key] = '5';
         break;
-    
       default:
         break;
     }
@@ -97,7 +97,7 @@ function encode(string) {
   return splited.join('');
 }
 function decode(string) {
-  var splited = string.split('');
+  let splited = string.split('');
 
   for (let key in splited) {
     switch (splited[key]) {
@@ -114,16 +114,16 @@ function decode(string) {
         splited[key] = 'o';
         break;
       case '5':
-        splited[key] = "u";
+        splited[key] = 'u';
         break;
     
       default:
         break;
     }
   }
-
   return splited.join('');;
 }
+
 
 module.exports = {
   calcArea,
