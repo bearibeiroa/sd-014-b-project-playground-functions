e9ce265aba3d29d905bed2b8f8160f3ff5cf6933
S4bot4 <sabotamail@gmail.com>
2021-07-06 02:17:49 -0300

    Cumprindo requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,28 +1,19 @@
 // Desafio 10
-function techList(nomeTech, name) {
-  
+function techList(tech, name) {
   // criar um array vazio
- // criar um objeto vazio
- // o for vai jogar dentro do objeto
- 
-  let nameListTech = {
-    tech: 'CSS',
-    name: 'Thiago',
-    tech: 'Html',
-    name: 'Thiago',
-    tech: 'JavaScript',
-    name: 'Thiago',
-    tech: 'Jest',
-    name: 'Thiago',
-    tech: 'React',
-    name: 'Thiago',
-};
-  
-  for (let index in nameListTech) {
-    return (index, nameListTech[index]);
+  // criar um objeto vazio
+  // o for vai jogar dentro do objeto
+     tech.sort();
+     let listOfTechs = [];
+  if (tech.length === 0) {
+    return 'Vazio!';
   }
+  for (let index of tech) {
+    listOfTechs.push({'tech':index, 'name':name});
+    
+  }
+  return listOfTechs;
 }
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
