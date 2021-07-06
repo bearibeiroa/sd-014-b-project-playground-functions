96b6ec87f0eae2b7a78e8f408b711fb8bb0e1790
Paola Gay <paolagaydossantos@gmail.com>
2021-07-05 22:15:58 -0300

    Function 11 - done!

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,10 +4,35 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(cellnumber) {
+  if (cellnumber.length != 11) {
+    return 'Array com tamanho incorreto.';  
+  }  
+
+  let stringFinal = '(' 
+  for (let i = 0; i < cellnumber.length; i += 1){
+    let contador = 0;
+    for (let j = 0; j < cellnumber.length; j += 1){
+      if (cellnumber[i] === cellnumber[j]){
+        contador += 1;
+      }
+    } 
+    if (contador >= 3 || cellnumber[i] > 9 || cellnumber[i] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores'            
+    } 
+    stringFinal += cellnumber[i]
+    if (i === 1){
+      stringFinal += ') ';
+    }
+    if (i === 6){
+        stringFinal += '-';
+    } 
+  }
+  return stringFinal;      
 }
 
+let opa = [1,1,1,9,2,4,5,6,7,8,9]
+console.log(generatePhoneNumber(opa)) 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
 let result = true;
