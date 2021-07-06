88c7573af39927e3f4b3daced59446cb73277cf7
vinicius23-dev <nicius.cadedes@gmail.com>
2021-07-06 10:07:11 -0300

    reaper

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,81 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  /*let resultado;
+  for (let index in tech, name) {
+    console.log(tech, name[index]);
+  }*/
+
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numero) {
   // seu código aqui
+  let contNumNeg = 0;
+  let contMaiorNove = 0;
+  let resultado = [];
+  let resposta;
+  let numeroTel = [];
+  let contx = 0;
+
+  for (let index in numero) {
+    resultado.push(numero[index]);
+
+    if(contx == 0){
+      numeroTel.push("(");
+      numeroTel.push(numero[index]);
+      contx += 1;
+      
+    } else if(contx == 3){
+      numeroTel.push(")");
+      numeroTel.push(numero[index]);
+      contx += 1;
+  
+    } else if(contx == 4){
+      numeroTel.push(" ");
+      numeroTel.push(numero[index]);
+      contx += 1;
+  
+    } else if(contx == 9){
+      numeroTel.push("-");
+      numeroTel.push(numero[index]);
+      contx += 1;
+  
+    } else {
+      numeroTel.push(numero[index]);
+      
+    }
+    contx += 1;
+    
+    
+    if(resultado[index] < 0){ //contador de números menores que zero
+      contNumNeg += 1;
+    } else if(resultado[index] > 9){ //contador de números maiores que nove
+      contMaiorNove += 1;
+    } else {
+
+    
+    }
+  }
+  
+    if(resultado.length != 11){ //verificando se o array tenha o tamanho diferente de 11
+      resposta = "Array com tamanho incorreto.";
+      
+    }
+
+    else if(contMaiorNove >= 1){ //verificando se há algum n° maior que 9
+      resposta = "não é possível gerar um número de telefone com esses valores";
+  
+    } else if(contNumNeg >= 1) { //verificando se há algum n° negativo
+      resposta = "não é possível gerar um número de telefone com esses valores";
+    } else {
+      resposta = numeroTel;
+    }
+  
+    return resposta;
+    
+    
+
 }
 
 // Desafio 12
