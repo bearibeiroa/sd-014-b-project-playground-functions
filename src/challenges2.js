95e9e9336ce544532182a248ef9ea8eeae9fe108
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-05 23:07:25 -0300

    Requesito número 13.

--- src/challenges2.js
+++ src/challenges2.js
@@ -74,8 +74,22 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+
+function hydrate(drinks) {
+  let quantDrinks = drinks.replace(/\D/gim, '');
+  let sum = 0;
+
+  for (let index = 0; index < quantDrinks.length; index += 1) {
+    let quant = parseInt(quantDrinks[index]);
+    if (quant !== 0) {
+      sum += quant;
+    }
+  }
+  if (sum === 1) {
+  return sum + " copo de água"
+  } else {
+    return sum + " copos de água"
+  }
 }
 
 module.exports = {
