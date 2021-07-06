468ac7d32bfc5b996653d637eae4fdeedc516280
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-07-06 11:21:04 -0300

    testando

--- src/challenges2.js
+++ src/challenges2.js
@@ -28,23 +28,7 @@ function triangleCheck() {
 
 // Desafio 13
 function hydrate() {
-function hydrate(string) {
-  let matches = string.match(/(\d+)/g);
-  let matchesNum = [];
-  for (let index = 0; index < matches.length; index += 1) {
-    matchesNum[index] = +matches[index];
-  }
-  let sum = 0;
-  for (let index = 0; index < matches.length; index += 1) {
-    sum += matchesNum[index];
-  }
-  if (sum === 1) {
-    return `${sum} copo de água`;
-  }
-  return `${sum} copos de água`;
- }
-}
-
+ 
 module.exports = {
   generatePhoneNumber,
   techList,
