174440e0d1cf069f815ebb6ef6e00fdf4816b7a5
Gustavo <gus.inacio@hotmail.com>
2021-07-05 22:38:11 -0300

    Requisito #8

--- src/challenges.js
+++ src/challenges.js
@@ -54,8 +54,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let array = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 3 === 0 && numbers[i] % 5 !== 0) {
+      array.push('fizz')
+    } else if (numbers[i] % 5 === 0 && numbers[i] % 3 !== 0) {
+      array.push('buzz');
+    } else if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
+      array.push('fizzbuzz')
+    } else {
+      array.push('bug!')
+    }
+  }
+  return array;
 }
 
 // Desafio 9
