9c57ba890cf72d44e33ca825d5c4ada59805d684
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-07-20 19:46:15 -0300

    adicionando item 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,8 +19,31 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(phone) {
   // seu código aqui
+  let number = [...phone];
+  let num = ["(", number[0], number[1],")", " ", number[2], number[3], number[4], number[5], number[6], "-", number[7], number[8], number[9], number[10]];
+  if (number.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    for (let i = 0; i < number.length; i += 1) {
+      if (number[i] < 0 || number[i] > 9 ) {
+        return 'não é possível gerar um número de telefone com esses valores' ;
+      }
+    }
+    for (let i = 0; i < number.length; i += 1) {
+      let repNum = 0;
+      for (let index = 0; index < number.length; index += 1) {
+        if (number[i] === number[index]) {
+          repNum += 1;
+        }
+      }
+      if (repNum > 2) {
+        return "não é possível gerar um número de telefone com esses valores"
+      }
+    }
+  }
+  return num.join('',',');
 }
 
 // Desafio 12
